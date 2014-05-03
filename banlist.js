21fb07533120c77570a732a52834d00a2b70fe0d
Dakota Schneider <dakota@hackthetruth.org>
2014-05-03 02:14:02 -0700

    banlist: use $.log() instead of pre-toolbox debug module

--- banlist.js
+++ banlist.js
@@ -25,13 +25,7 @@ function banlist () {
     spinScript.textContent = '//fgnass.github.com/spin.js#v1.3.3\n'+
     '!function(a,b){"object"==typeof exports?module.exports=b():"function"==typeof define&&define.amd?define(b):a.Spinner=b()}(this,function(){"use strict";function a(a,b){var c,d=document.createElement(a||"div");for(c in b)d[c]=b[c];return d}function b(a){for(var b=1,c=arguments.length;c>b;b++)a.appendChild(arguments[b]);return a}function c(a,b,c,d){var e=["opacity",b,~~(100*a),c,d].join("-"),f=.01+c/d*100,g=Math.max(1-(1-a)/b*(100-f),a),h=k.substring(0,k.indexOf("Animation")).toLowerCase(),i=h&&"-"+h+"-"||"";return m[e]||(n.insertRule("@"+i+"keyframes "+e+"{0%{opacity:"+g+"}"+f+"%{opacity:"+a+"}"+(f+.01)+"%{opacity:1}"+(f+b)%100+"%{opacity:"+a+"}100%{opacity:"+g+"}}",n.cssRules.length),m[e]=1),e}function d(a,b){var c,d,e=a.style;for(b=b.charAt(0).toUpperCase()+b.slice(1),d=0;d<l.length;d++)if(c=l[d]+b,void 0!==e[c])return c;return void 0!==e[b]?b:void 0}function e(a,b){for(var c in b)a.style[d(a,c)||c]=b[c];return a}function f(a){for(var b=1;b<arguments.length;b++){var c=arguments[b];for(var d in c)void 0===a[d]&&(a[d]=c[d])}return a}function g(a){for(var b={x:a.offsetLeft,y:a.offsetTop};a=a.offsetParent;)b.x+=a.offsetLeft,b.y+=a.offsetTop;return b}function h(a,b){return"string"==typeof a?a:a[b%a.length]}function i(a){return"undefined"==typeof this?new i(a):(this.opts=f(a||{},i.defaults,o),void 0)}function j(){function c(b,c){return a("<"+b+\' xmlns="urn:schemas-microsoft.com:vml" class="spin-vml">\',c)}n.addRule(".spin-vml","behavior:url(#default#VML)"),i.prototype.lines=function(a,d){function f(){return e(c("group",{coordsize:k+" "+k,coordorigin:-j+" "+-j}),{width:k,height:k})}function g(a,g,i){b(m,b(e(f(),{rotation:360/d.lines*a+"deg",left:~~g}),b(e(c("roundrect",{arcsize:d.corners}),{width:j,height:d.width,left:d.radius,top:-d.width>>1,filter:i}),c("fill",{color:h(d.color,a),opacity:d.opacity}),c("stroke",{opacity:0}))))}var i,j=d.length+d.width,k=2*j,l=2*-(d.width+d.length)+"px",m=e(f(),{position:"absolute",top:l,left:l});if(d.shadow)for(i=1;i<=d.lines;i++)g(i,-2,"progid:DXImageTransform.Microsoft.Blur(pixelradius=2,makeshadow=1,shadowopacity=.3)");for(i=1;i<=d.lines;i++)g(i);return b(a,m)},i.prototype.opacity=function(a,b,c,d){var e=a.firstChild;d=d.shadow&&d.lines||0,e&&b+d<e.childNodes.length&&(e=e.childNodes[b+d],e=e&&e.firstChild,e=e&&e.firstChild,e&&(e.opacity=c))}}var k,l=["webkit","Moz","ms","O"],m={},n=function(){var c=a("style",{type:"text/css"});return b(document.getElementsByTagName("head")[0],c),c.sheet||c.styleSheet}(),o={lines:12,length:7,width:5,radius:10,rotate:0,corners:1,color:"#000",direction:1,speed:1,trail:100,opacity:.25,fps:20,zIndex:2e9,className:"spinner",top:"auto",left:"auto",position:"relative"};i.defaults={},f(i.prototype,{spin:function(b){this.stop();var c,d,f=this,h=f.opts,i=f.el=e(a(0,{className:h.className}),{position:h.position,width:0,zIndex:h.zIndex}),j=h.radius+h.length+h.width;if(b&&(b.insertBefore(i,b.firstChild||null),d=g(b),c=g(i),e(i,{left:("auto"==h.left?d.x-c.x+(b.offsetWidth>>1):parseInt(h.left,10)+j)+"px",top:("auto"==h.top?d.y-c.y+(b.offsetHeight>>1):parseInt(h.top,10)+j)+"px"})),i.setAttribute("role","progressbar"),f.lines(i,f.opts),!k){var l,m=0,n=(h.lines-1)*(1-h.direction)/2,o=h.fps,p=o/h.speed,q=(1-h.opacity)/(p*h.trail/100),r=p/h.lines;!function s(){m++;for(var a=0;a<h.lines;a++)l=Math.max(1-(m+(h.lines-a)*r)%p*q,h.opacity),f.opacity(i,a*h.direction+n,l,h);f.timeout=f.el&&setTimeout(s,~~(1e3/o))}()}return f},stop:function(){var a=this.el;return a&&(clearTimeout(this.timeout),a.parentNode&&a.parentNode.removeChild(a),this.el=void 0),this},lines:function(d,f){function g(b,c){return e(a(),{position:"absolute",width:f.length+f.width+"px",height:f.width+"px",background:b,boxShadow:c,transformOrigin:"left",transform:"rotate("+~~(360/f.lines*j+f.rotate)+"deg) translate("+f.radius+"px,0)",borderRadius:(f.corners*f.width>>1)+"px"})}for(var i,j=0,l=(f.lines-1)*(1-f.direction)/2;j<f.lines;j++)i=e(a(),{position:"absolute",top:1+~(f.width/2)+"px",transform:f.hwaccel?"translate3d(0,0,0)":"",opacity:f.opacity,animation:k&&c(f.opacity,f.trail,l+j*f.direction,f.lines)+" "+1/f.speed+"s linear infinite"}),f.shadow&&b(i,e(g("#000","0 0 4px #000"),{top:"2px"})),b(d,b(i,g(h(f.color,j),"0 0 1px rgba(0,0,0,.1)")));return d},opacity:function(a,b,c){b<a.childNodes.length&&(a.childNodes[b].style.opacity=c)}});var p=e(a("group"),{behavior:"url(#default#VML)"});return!d(p,"transform")&&p.adj?j():k=d(p,"animation"),i});';
     document.getElementsByTagName('head')[0].appendChild(spinScript);
-     
-    // add debug script
-    debugScript = document.createElement('script');
-    debugScript.type = "text/javascript";
-    debugScript.textContent = 'window.debug=(function(){var i=this,b=Array.prototype.slice,d=i.console,h={},f,g,m=9,c=["error","warn","info","debug","log"],l="assert clear count dir dirxml exception group groupCollapsed groupEnd profile profileEnd table time timeEnd trace".split(" "),j=l.length,a=[];while(--j>=0){(function(n){h[n]=function(){m!==0&&d&&d[n]&&d[n].apply(d,arguments)}})(l[j])}j=c.length;while(--j>=0){(function(n,o){h[o]=function(){var q=b.call(arguments),p=[o].concat(q);a.push(p);e(p);if(!d||!k(n)){return}d.firebug?d[o].apply(i,q):d[o]?d[o](q):d.log(q)}})(j,c[j])}function e(n){if(f&&(g||!d||!d.log)){f.apply(i,n)}}h.setLevel=function(n){m=typeof n==="number"?n:9};function k(n){return m>0?m>n:c.length+m<=n}h.setCallback=function(){var o=b.call(arguments),n=a.length,p=n;f=o.shift()||null;g=typeof o[0]==="boolean"?o.shift():false;p-=typeof o[0]==="number"?o.shift():n;while(p<n){e(a[p++])}};return h})();';
-    document.getElementsByTagName('head')[0].appendChild(debugScript);
-
+    
     /*
      * credit: https://gist.github.com/its-florida/1290439/
      * You can now create a spinner using any of the variants below:
@@ -87,14 +81,7 @@ function banlist () {
     // from http://stackoverflow.com/a/15780907/362042
     function getURLParameter(url, name) {
         return (RegExp(name + '=' + '(.+?)(&|$)').exec(url)||[,null])[1];
-    }
- 
-    // debug only in dev mode
-    if (TBUtils.setting('Utils', 'debugMode', true)) {
-        debug.setLevel(5);
-    } else {
-        debug.setLevel(0);
-    }
+     
  
     banlist_updating = false;
     banlist_last_update = 0;
@@ -108,7 +95,7 @@ function banlist () {
         after      = typeof after      !== 'undefined' ? after      : '';
         pages_back = typeof pages_back !== 'undefined' ? pages_back : 0;
  
-        debug.info("_get_next_ban_page("+after+")");
+        $.log("_get_next_ban_page("+after+")");
  
         var parameters = {'limit': 1000, 'after': after};
  
@@ -125,12 +112,12 @@ function banlist () {
             dataType: 'html',
             async: true,
             success: function(data) {
-                debug.info("  success!");
-                debug.info("  "+pages_back+" pages back");
+                $.log("  success!");
+                $.log("  "+pages_back+" pages back");
                 var response_page = $(data);
                 // append to the list, using clever jQuery context parameter to create jQuery object to parse out the HTML response
                 // var $new_banlist = $('.banned-table', response_page);
-                debug.info($('.banned-table table tbody tr', response_page).length);
+                $.log($('.banned-table table tbody tr', response_page).length);
                 if ($('.banned-table table tbody tr', response_page).length > 0) {
                     $('.banned-table table tbody tr', response_page).each(function() {
                         // workaround for known bug in listings where "next" button is available on last page
@@ -151,9 +138,9 @@ function banlist () {
                 }
 
                 after_url = $('.nextprev a[rel~="next"]', response_page).prop('href');
-                debug.info(after_url);
+                $.log(after_url);
                 after = getURLParameter(after_url, 'after');
-                debug.info(after);
+                $.log(after);
                 if (after) {
                     // hit the API hard the first 10, to make it more responsive on small subs
                     if (pages_back < 10) {
@@ -164,15 +151,15 @@ function banlist () {
                         setTimeout(_get_next_ban_page, sleep, after, pages_back);
                     }
                 } else {
-                    debug.info("  last page");
+                    $.log("  last page");
                     banlist_updating = false;
                     banlist_last_update = Date.now();
                     $loading.hide();
                 }
             },
             error: function(data) {
-                debug.info("  failed");
-                debug.info(data.status);
+                $.log("  failed");
+                $.log(data.status);
                 if (data.status == 504) {
                     // "504, post some more"
                     this.success(data);
@@ -188,21 +175,21 @@ function banlist () {
     }
  
     function filter_banlist(banlist, value, ignore_last) {
-        debug.info('filter('+value+')');
+        $.log('filter('+value+')');
         last_value = typeof last_value !== 'undefined' ? last_value : '';
         ignore_last = typeof ignore_last !== 'undefined' ? ignore_last : false;
 
         if ( value == '' ) {
-            debug.info('empty');
+            $.log('empty');
             // empty search? show all
             $('tr', banlist).show().addClass('visible');
         } else if ( !ignore_last && last_value && value.indexOf(last_value) > -1 ) {
-            debug.info('subset');
+            $.log('subset');
             // is this query a subset of the last query?
             // filter *out* non-matching
             $("tr.visible .indexColumn:not(:contains('" + value + "'))", banlist).parent().hide().removeClass('visible');
         } else {
-            debug.info('full search');
+            $.log('full search');
             $('tr', banlist).hide().removeClass('visible');
             // combine and use a single selector for increased performance
             // credit: http://kobikobi.wordpress.com/2008/09/15/using-jquery-to-filter-table-rows/

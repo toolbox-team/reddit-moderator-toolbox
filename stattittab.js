f0d2e26f4be99b54535184cbb3d06e1c6b880230
Dakota Schneider <dakota@hackthetruth.org>
2014-05-09 23:01:07 -0700

    port stattit tab to Toolbox Object

--- stattittab.js
+++ stattittab.js
@@ -10,50 +10,67 @@
 // @copyright  2013+, LowSociety
 // ==/UserScript==
 
-var stattitTab = {
+function stattitTabWrapper() {
+
+var stattitTab = new Toolbox.TBModule('Stattit Tab', '0.1');
     
-    getSectionFromUrl : function(url) {
-        var regex = new RegExp(/^(http|https):\/\/([a-z]+\.)?reddit\.com\/(user|r)\/([^\/]+)(\/|$)/g);
-        var matches = regex.exec(url);
-        
-        if(matches != null)
-            return { section : matches[3], subSection : matches[4] };
-        else
-            return null;
-    },
+stattitTab.getSectionFromUrl = function(url) {
+    var regex = new RegExp(/^(http|https):\/\/([a-z]+\.)?reddit\.com\/(user|r)\/([^\/]+)(\/|$)/g);
+    var matches = regex.exec(url);
     
-    init : function() {
+    if(matches != null) {
+        return { section : matches[3], subSection : matches[4] };
+    } else {
+        return null;
+    }
+};
+    
+stattitTab.init = function() {
     var page = this.getSectionFromUrl(window.location.href);
         
-        if(page == null)
-            return false;
-        
-        var header = document.getElementById("header-bottom-left");
-        var tabList = header.getElementsByTagName("ul")[0];
-        
-        if (tabList == null)
-            return false;
-        
-        var listItem = document.createElement("li");
-        var link = document.createElement("a");
-
-        if (page.section === 'user') {
-            link.href = "http://karmawhores.net/" + page.section + "/" + page.subSection;
-            link.innerHTML = "karma stats";
-        } else {
-            link.href = "http://redditmetrics.com/" + page.section + "/" + page.subSection;
-            link.innerHTML = "reddit metrics";
-        }
-
-        link.target = "_blank";
-        listItem.appendChild(link);
-        tabList.appendChild(listItem);
+    if(page == null) {
+        return false;
     }
     
-}
+    var header = document.getElementById("header-bottom-left");
+    var tabList = header.getElementsByTagName("ul")[0];
+    
+    if (tabList == null) {
+        return false;
+    }
+    
+    var listItem = document.createElement("li");
+    var link = document.createElement("a");
 
-if (document.body) {
-  if (JSON.parse(localStorage['Toolbox.StattitTab.enabled'] || 'true')) {
-      stattitTab.init();  
-  }
-}
+    if (page.section === 'user') {
+        link.href = "http://karmawhores.net/" + page.section + "/" + page.subSection;
+        link.innerHTML = "karma stats";
+    } else {
+        link.href = "http://redditmetrics.com/" + page.section + "/" + page.subSection;
+        link.innerHTML = "reddit metrics";
+    }
+
+    link.target = "_blank";
+    listItem.appendChild(link);
+    tabList.appendChild(listItem);
+};
+
+stattitTab.settings_defaults['enabled'] = true;    
+
+
+Toolbox.register_module(stattitTab);
+
+} // end of stattitTabWrapper()
+
+// if (document.body) {
+//   if (JSON.parse(localStorage['Toolbox.StattitTab.enabled'] || 'true')) {
+//       stattitTab.init();  
+//   }
+// }
+
+// Add script to page
+(function () {
+    var s = document.createElement('script');
+    s.textContent = "(" + stattitTabWrapper.toString() + ')();';
+    document.head.appendChild(s);
+})();

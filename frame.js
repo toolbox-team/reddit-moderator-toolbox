c01635a455ee8d5800cdbf573055db7855ed95d1
Neil Layne <n.layne@gmail.com>
2014-05-12 18:05:43 -0400

    Several updates to mod frame.
    Can now replace the mod bar buttons, load limit or count, page behind no
    longer scrolls.

--- frame.js
+++ frame.js
@@ -17,20 +17,42 @@
 
     $.log('Loading Frame Module');
 
-    var $html, limit = 10;
+    var $html, limit = 10, jackModBarLinks = false,
+        msgGet = limit, mmGet = limit, modGet = limit, umodGet = limit;
 
     // Hijack modbar.
-    $('#tb-bottombar').find('#tb-toolbarcounters').append('\
+    if (!jackModBarLinks) {
+        $('#tb-bottombar').find('#tb-toolbarcounters').append('\
             <span>&nbsp;&nbsp;&nbsp;<a href="javascript:;" id="tb-launch-fame">[F]</a></span>\
             ');
+    }
+    else {
+        // Hack-y hack.  To be removed.
+        setTimeout(function () {
+            $('#tb-mailCount').attr("href", "#").addClass('tb-launch-fame');
+            $('#tb-modmailcount').attr("href", "#").addClass('tb-launch-fame');
+            $('#tb-queueCount').attr("href", "#").addClass('tb-launch-fame');
+            $('#tb-unmoderatedcount').attr("href", "#").addClass('tb-launch-fame');
+        }, 500);
+    }
 
-    $('body').delegate('#tb-launch-fame', 'click', function () {
+    $('body').delegate('#tb-launch-fame, .tb-launch-fame', 'click', function (e) {
+
         var unreadMessageCount = TBUtils.getSetting('Notifier', 'unreadmessagecount', 0),
             modqueueCount = TBUtils.getSetting('Notifier', 'modqueuecount', 0),
             unmoderatedCount = TBUtils.getSetting('Notifier', 'unmoderatedcount', 0),
             modmailCount = TBUtils.getSetting('Notifier', 'modmailcount', 0);
 
-        // Need to fix centering, etc.
+        
+        msgGet = (unreadMessageCount <= limit && unreadMessageCount > 0) ? unreadMessageCount : limit;
+        $.log(msgGet);
+        mmGet = (modmailCount <= limit && modmailCount > 0) ? modmailCount : limit;
+        $.log(mmGet);
+        modGet = (modqueueCount <= limit && modqueueCount > 0) ? modqueueCount : limit;
+        $.log(modGet);
+        umodGet = (unmoderatedCount <= limit && unmoderatedCount > 0) ? unmoderatedCount : limit;
+        $.log(umodGet);
+
         $html = $('\
             <div class="tb-page-overlay tb-frame-module">\
                 <div class="tb-window-wrapper">\
@@ -41,8 +63,8 @@
                         </span>\
                     </div>\
                     <div class="tb-window-tabs">\
-                        <a href="javascript:;" title="View Mod Mail" class="tb-frame-modmail">Mod Mail ('+ modmailCount +')</a>\
                         <a href="javascript:;" title="View Messages" class="tb-frame-messages">Messages ('+ unreadMessageCount +')</a>\
+                        <a href="javascript:;" title="View Mod Mail" class="tb-frame-modmail">Mod Mail ('+ modmailCount +')</a>\
                         <a href="javascript:;" title="View Mod Queue" class="tb-frame-modqueue">Mod Queue ('+ modqueueCount +')</a>\
                         <a href="javascript:;" title="View Unmoderated" class="tb-frame-unmoderated">Unmoderated ('+ unmoderatedCount +')</a>\
                     </div>\
@@ -54,56 +76,49 @@
                 </div>\
             </div>\
         ').appendTo('body').show();
-
-        /*
-        $html = $('\
-            <div class="mod-popup tb-page-overlay tb-settings">\
-                <div class="mod-popup-header">\
-                    Tool Box Frame\
-                    <span class="close right">\
-                        <a href="javascript:;">[x]</a>\
-                    </span>\
-                </div>\
-                <div class="mod-popup-tabs">\
-                    <a href="javascript:;" title="View Mod Mail" class="tb-frame-modmail">Mod Mail</a>\
-                    <a href="javascript:;" title="View Messages" class="tb-frame-messages">Messages</a>\
-                    <a href="javascript:;" title="View Mod Queue" class="tb-frame-modqueue">Mod Queue</a>\
-                    <a href="javascript:;" title="View Unmoderated" class="tb-frame-unmoderated">Unmoderated</a>\
-                </div>\
-                <div class="mod-popup-content">\
-                Select a tab\
-                </div>\
-                <div class="mod-popup-footer">\
-                    <span class="footer-button-one">\
-                    </span>\
-                </div>\
-            </div>\
-        ').appendTo('body').show();
-        */
-    });
-
-    $('body').delegate('.tb-frame-modmail', 'click', function () {
-        update('http://www.reddit.com/message/moderator/?limit=' + limit);
-    });
-
-    $('body').delegate('.tb-frame-messages', 'click', function () {
-        update('http://www.reddit.com/message/inbox/?limit=' + limit);
-    });
-
-    $('body').delegate('.tb-frame-modqueue', 'click', function () {
-        update('http://www.reddit.com/r/mod/about/modqueue/?limit=' + limit);
-    });
-
-    $('body').delegate('.tb-frame-unmoderated', 'click', function () {
-        update('http://www.reddit.com/r/mod/about/unmoderated/?limit=' + limit);
+        $('body').css('overflow', 'hidden');
+        
+        switch (e.target.id) { 
+            case 'tb-mailCount':
+                update('http://www.reddit.com/message/inbox/?limit=' + msgGet);
+                break;
+            case 'tb-modmailcount':
+                update('http://www.reddit.com/message/moderator/?limit=' + mmGet);
+                break;
+            case 'tb-queueCount':
+                update('http://www.reddit.com/r/mod/about/modqueue/?limit=' + modGet);
+                break;
+            case 'tb-unmoderatedcount':
+                update('http://www.reddit.com/r/mod/about/unmoderated/?limit=' + umodGet);
+                break;
+            default:
+                break;
+        }
     });
 
     $('body').delegate('.tb-frame-close', 'click', function () {
-        $html.remove();
+            $html.remove();
+            $('body').css('overflow', 'auto');
+    });
+
+    $('body').delegate('.tb-frame-messages', 'click', function () {
+        update('http://www.reddit.com/message/inbox/?limit=' + msgGet);
+    });
+
+    $('body').delegate('.tb-frame-modmail', 'click', function () {
+        update('http://www.reddit.com/message/moderator/?limit=' + mmGet);
+    });
+
+    $('body').delegate('.tb-frame-modqueue', 'click', function () {
+        update('http://www.reddit.com/r/mod/about/modqueue/?limit=' + modGet);
+    });
+
+    $('body').delegate('.tb-frame-unmoderated', 'click', function () {
+        update('http://www.reddit.com/r/mod/about/unmoderated/?limit=' + umodGet);
     });
 
     function update(URL) {
-        $html.find('.tb-window-content').html('Loading...');
+        $html.find('.tb-window-content').html('Loading... ' + URL);
         $.get(URL, function (resp) {
             if (!resp) return;
 

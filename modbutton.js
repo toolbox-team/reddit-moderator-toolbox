toolbox-team/reddit-moderator-toolbox

commit 621d094aafa43d92726d8511aa205a8447230beb

add confirmation for modbutton
modbutton now asks for confirmation when performing any action on more
than one subreddit.

modbutton.js
@@ -347,11 +347,18 @@
                 }
             });
 
-            // Ban
-            massAction(subreddits);
- 
             // Check dem values.
             if (subreddits.length < 1) return $status.text('error, no subreddits selected');
+
+            if (subreddits.length > 1) {
+                var confirmban = confirm("This will " + actionName + " /u/" + user + " from " + subreddits.join(', ') + ".   Are you sure?");
+                if (confirmban) {
+                    // Ban
+                    massAction(subreddits);
+                } else {
+                    return;
+                }
+            }
         } else {
             var confirmban = confirm("This will " + actionName + " /u/" + user + " from every subreddit you moderate.   Are you sure?");
             if (confirmban) {

commit 433b92c52d68ec635e4f0baa27bf37cf1e70a21e

fix 621d094
621d094 broke mod button so that it would only action of multiple subs
were selected, this fixes that.

modbutton.js
@@ -319,7 +319,7 @@
  
     // 'save' button clicked...  THIS IS WHERE WE BAN PEOPLE, PEOPLE!
     $('body').on('click', '.mod-popup .save, .global-button', function () {
- 
+
         var $button = $(this),
             $popup = $button.parents('.mod-popup'),
             $selected = $popup.find('.mod-action :selected'),
@@ -337,7 +337,7 @@
         if (!api) return $status.text('error, no action selected');
 
         if (!$(this).hasClass('global-button')) {
- 
+
             // Get dem ban subs.
             $popup.find('.action-sub:checkbox:checked').each(function () {
                 if ($(this).val() !== OTHER) {
@@ -355,6 +355,7 @@
             if (subreddits.length < 1) return $status.text('error, no subreddits selected');
 
             if (subreddits.length > 1) {
+                // require confirmation on all multi-sub actions
                 var confirmban = confirm("This will " + actionName + " /u/" + user + " from " + subreddits.join(', ') + ".   Are you sure?");
                 if (confirmban) {
                     // Ban
@@ -362,6 +363,9 @@
                 } else {
                     return;
                 }
+            } else {
+                // must be only one sub selected, no need to confirm
+                massAction(subreddits);
             }
         } else {
             var confirmban = confirm("This will " + actionName + " /u/" + user + " from every subreddit you moderate.   Are you sure?");
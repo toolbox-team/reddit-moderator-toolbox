6e851cdb1dd41010fb992715ea8ad4957d796a1e
creesch <creesch.r@gmail.com>
2014-06-24 22:00:57 +0200

    comment.js code cleaning and fixing

--- comment.js
+++ comment.js
@@ -19,32 +19,12 @@
     // preload some generic variables 
     //
     var hideRemoved = TBUtils.getSetting('CommentsMod', 'hideRemoved', false),
-        highlighted = TBUtils.getSetting('CommentsMod', 'highlighted', ''),
         approveComments = TBUtils.getSetting('CommentsMod', 'approvecomments', false),
         spamRemoved = TBUtils.getSetting('CommentsMod', 'spamremoved', false),
         hamSpammed = TBUtils.getSetting('CommentsMod', 'hamspammed', false);
 
 
-    //
-    //  Do stuff with removed comments
-    //
-    // Show a removed comments counter when visiting a comment page on a sub where you are moderator. When hiding of removed comments is enabled this doubles as a toggle for that.
-    var removedCounter = 0;
 
-    $('.comments-page .thing.comment.spam > .entry').each(function () {
-        $(this).addClass('tb-comment-spam');
-        removedCounter = removedCounter + 1;
-    });
-    $.log(removedCounter, true);
-    if (removedCounter == 1) {
-        $('#tb-bottombar').find('#tb-toolbarcounters').prepend('<a id="tb-toggle-removed" title="Toggle hide/view removed comments" href="javascript:void(0)"><img src="data:image/png;base64,' + TBui.iconCommentsRemove + '" />[1]</a>');
-    } else if (removedCounter > 1) {
-        $('#tb-bottombar').find('#tb-toolbarcounters').prepend('<a id="tb-toggle-removed" title="Toggle hide/view removed comments" href="javascript:void(0)"><img src="data:image/png;base64,' + TBui.iconCommentsRemove + '" />[' + removedCounter.toString() + ']</a>');
-    }
-
-    if (hideRemoved) {
-        $('.tb-comment-spam').hide();
-    }
 
     $('body').on('click', '#tb-toggle-removed', function () {
         if ($('.tb-comment-spam').is(':visible')) {
@@ -56,10 +36,28 @@
     });
 
     function run() {
-        if (approveComments
-            || spamRemoved
-            || hamSpammed
-        ) {
+
+        //
+        //  Do stuff with removed comments
+        //
+        // Show a removed comments counter when visiting a comment page on a sub where you are moderator. When hiding of removed comments is enabled this doubles as a toggle for that.
+        var removedCounter = 0;
+
+        $('.comments-page .thing.comment.spam > .entry').each(function () {
+            $(this).addClass('tb-comment-spam');
+            removedCounter = removedCounter + 1;
+        });
+        $.log(removedCounter, true);
+        if (removedCounter == 1) {
+            $('#tb-bottombar').find('#tb-toolbarcounters').prepend('<a id="tb-toggle-removed" title="Toggle hide/view removed comments" href="javascript:void(0)"><img src="data:image/png;base64,' + TBui.iconCommentsRemove + '" />[1]</a>');
+        } else if (removedCounter > 1) {
+            $('#tb-bottombar').find('#tb-toolbarcounters').prepend('<a id="tb-toggle-removed" title="Toggle hide/view removed comments" href="javascript:void(0)"><img src="data:image/png;base64,' + TBui.iconCommentsRemove + '" />[' + removedCounter.toString() + ']</a>');
+        }
+
+        if (hideRemoved) {
+            $('.tb-comment-spam').hide();
+        }
+        if (approveComments || spamRemoved || hamSpammed) {
             // only need to iterate if at least one of the options is enabled
             $('.thing.comment').each(function () {
                 if (!$(this).hasClass('.tb-comments-checked')) {
@@ -70,9 +68,7 @@
                     if (approveComments) {
                         // only for subreddits we mod
                         // and for comments that haven't already been approved
-                        if (thing.subreddit
-                            && !thing.approved_by
-                        ) {
+                        if (thing.subreddit && !thing.approved_by) {
                             // and only if there isn't already one
                             if ($(this).children('.entry').find('.buttons .positive').length == 0) {
                                 // lifted straight from the "remove" link button
@@ -85,9 +81,7 @@
                     if (spamRemoved) {
                         // only for subreddits we mod
                         // and for comments that have been removed as ham ("remove not spam")
-                        if (thing.subreddit
-                            && thing.ham
-                        ) {
+                        if (thing.subreddit && thing.ham) {
                             // and only if there isn't already one
                             if ($(this).children('.entry').find('.big-mod-buttons .negative').length == 0) {
                                 // lifted straight from the "spam" big mod button
@@ -102,9 +96,7 @@
                     if (hamSpammed) {
                         // only for subreddits we mod
                         // and for comments that have been removed as spam ("spam" or "confirm spam")
-                        if (thing.subreddit
-                            && thing.spam
-                        ) {
+                        if (thing.subreddit && thing.spam) {
                             // and only if there isn't already one
                             if ($(this).children('.entry').find('.big-mod-buttons .neutral').length == 0) {
                                 // lifted straight from the "remove" big mod button
@@ -122,10 +114,18 @@
             });
 
         }
+
+        if (TBUtils.getSetting('CommentsMod', 'highlighted', '')) {
+            var highlighted = TBUtils.getSetting('CommentsMod', 'highlighted', '');
+            highlighted = highlighted.split(',');
+
+            $('.md p').highlight(highlighted);
+        }
     }
     // NER support.
     window.addEventListener("TBNewThings", function () {
         run();
+
     });
 
     run();
@@ -134,33 +134,4 @@
 
 
 
-  //  $('.thing.comment.spam').click(function () {
-  //      var hiddenComment;
-   //     hiddenComment = $(this).find('.tb-comment-spam');
-
-  //      if (hiddenComment.is(':visible')) {
- //           hiddenComment.hide();
- //       } else {
- //           hiddenComment.show();
- //       }
- //   }).children().click(function (e) {
-//        return;
-//    });
-
-    // Add a bit of js based css since the hover effects otherwise will go to the child elements as well 
-    $('.thing.comment.spam').children().hover(function () {
-        $(this).css('cursor', 'auto');
-    });
-
-
-
-    //
-    //  Highlight words 
-    //	
-
-    if (highlighted) {
-        highlighted = highlighted.split(',');
-
-        $('.md p').highlight(highlighted);
-    }
 })();

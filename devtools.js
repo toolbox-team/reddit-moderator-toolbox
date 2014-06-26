2468f3c4f732675a3d265f81365f600a1cc6da61
Dakota Schneider <dakota@hackthetruth.org>
2014-06-26 10:11:30 -0700

    add devtools.js module

---
+++ devtools.js
@@ -0,0 +1,7 @@
+// Developer Tools, for managing /r/toolbox/w/tbnotes (etc.)
+
+var devTools = new TB.Module('Developer Tools');
+
+devTools.setting('betamode', false);
+devTools.setting('devmode', true);
+

6c56a017995caec1ff46efbb761a34d54992cb04
Dakota Schneider <dakota@hackthetruth.org>
2014-06-30 13:00:28 -0700

    hide ban stuff on about/mod and about/contributors

--- modbutton.js
+++ modbutton.js
@@ -180,7 +180,7 @@ modButton.init = function init() {
                         </div>\
                         <div class="ban-note-container"><input id="ban-note" class="ban-note" type="text" placeholder="(ban note)" maxlength="300"></input><br>\
 						<textarea name="ban-message" class="ban-message" placeholder="(ban message to user)" ></textarea><br>\
-						<input type="number" min="1" max="999" name="ban-duration"  class="ban-duration" placeholder="time (days)"> <span class="ban-span-include-time">Include in message <input type="checkbox" name="ban-include-time" class="ban-include-time" value="ban-include-time"></span>\
+                        <input type="number" min="1" max="999" name="ban-duration"  class="ban-duration" placeholder="time (days)"> <label class="ban-span-include-time"><input type="checkbox" name="ban-include-time" class="ban-include-time" value="ban-include-time"> Include in message </label>\
 						</div>',
                     footer: '\
                         <span class="status error left"></span>\
@@ -300,10 +300,18 @@ modButton.init = function init() {
             $popup.find('select.mod-action option[data-api=friend][data-action=moderator]').attr('selected', 'selected');
             $popup.find('.ban-note').hide();
             $popup.find('.action-sub:checkbox:checked').removeAttr('checked');
+
+            $popup.find('textarea.ban-message').hide();
+            $popup.find('.ban-duration').hide();
+            $popup.find('.ban-span-include-time').hide();
         } else if (location.pathname.match(/\/about\/(?:contributors)\/?/)) {
             $popup.find('select.mod-action option[data-api=friend][data-action=contributor]').attr('selected', 'selected');
             $popup.find('.ban-note').hide();
             $popup.find('.action-sub:checkbox:checked').removeAttr('checked');
+
+            $popup.find('textarea.ban-message').hide();
+            $popup.find('.ban-duration').hide();
+            $popup.find('.ban-span-include-time').hide();
         }
 
         // render the saved subs lists

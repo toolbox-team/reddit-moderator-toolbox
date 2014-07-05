db9d09dbe0e1034a2fa0ac5775b46caf918080e5
Dakota Schneider <dakota@hackthetruth.org>
2014-07-04 22:20:12 -0700

    fix config link injection point

--- modules/config.js
+++ modules/config.js
@@ -2,7 +2,7 @@ function tbconfig() {
     if (!TBUtils.logged || !TBUtils.getSetting('TBConfig', 'enabled', true)) return;
     $.log('Loading Configuration Module');
 
-    var toolbox = $('#moderation_tools').find('.content'),
+    var toolbox = $('#moderation_tools').find('.content .icon-menu'),
         //configLink = '<li><img src="data:image/png;base64,' + TBui.iconWrench + '"/><span class="separator"></span><a href="javascript:;" class="toolbox-edit" title="toolbox configuration for this subreddit">toolbox configuration</a></li>',
         configLink = '<li><a href="javascript:;" class="toolbox-edit" title="toolbox configuration for this subreddit">toolbox configuration</a></li>',
         subreddit = TBUtils.post_site,

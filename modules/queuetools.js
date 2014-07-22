8a58131e3ed0998b36fd455e7a9361e7864662f4
Dakota Schneider <dakota@hackthetruth.org>
2014-07-22 14:44:44 -0700

    oops

--- modules/queuetools.js
+++ modules/queuetools.js
@@ -625,7 +625,7 @@ function queuetools() {
     }
 
     if (($body.hasClass('listing-page') || $body.hasClass('comments-page')) && (!TBUtils.post_site || $('body.moderator').length)) {
-        $('<li><a href="javascript:;" accesskey="M" class="modtools-on">queue tools</a></li>').appendTo('.tabmenu').click(addModtools);
+        $('.tabmenu').first().append($('<li><a href="javascript:;" accesskey="M" class="modtools-on">queue tools</a></li>').click(addModtools));
     }
 }
 

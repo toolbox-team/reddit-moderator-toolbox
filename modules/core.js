57876a184710b62b798fba8f000720df7cf2d8f8
Neil Layne <n.layne@gmail.com>
2014-12-05 01:44:54 -0500

    gear icon?

--- modules/core.js
+++ modules/core.js
@@ -96,7 +96,7 @@ var shortLength = TB.storage.getCache('Core', 'shortlength', 15),
 var modbar = $('\
 <div id="tb-bottombar" class="tb-toolbar">\
     <a class="tb-bottombar-hide" href="javascript:void(0)"><img src="data:image/png;base64,' + TBui.iconHide + '" /></a>&nbsp;&nbsp;\
-    <a class="tb-toolbar tb-toolbarsettings" href="javascript:void(0)"><img src="data:image/png;base64,' + TBui.iconWrench + '" title="toolbox settings"/></a>\
+    <a class="tb-toolbar tb-toolbarsettings" href="javascript:void(0)"><img src="data:image/png;base64,' + TBui.iconGear + '" title="toolbox settings"/></a>\
     <span><label class="tb-first-run">&#060;-- Click for settings &nbsp;&nbsp;&nbsp;</label><span>\
     <span>&nbsp; | &nbsp;</span>\
     <span id="tb-toolbarshortcuts"></span>\

e06447348ca8341aed481f6ec2d37ca498a6d355
creesch <creesch.r@gmail.com>
2014-12-08 09:50:21 +0100

    String to color method

--- tbutils.js
+++ tbutils.js
@@ -1252,6 +1252,16 @@ function initwrapper() {
     TBUtils.removeQuotes = function (string) {
         return string.replace(/['"]/g, '');
     };
+    
+    TBUtils.stringToColor = function(str) {
+    // str to hash
+    for (var i = 0, hash = 0; i < str.length; hash = str.charCodeAt(i++) + ((hash << 5) - hash));
+
+    // int/hash to hex
+    for (var i = 0, color = "#"; i < 3; color += ("00" + ((hash >> i++ * 8) & 0xFF).toString(16)).slice(-2));
+
+    return color;
+    }
 
 
     // Added back for MMP's live mod mail.

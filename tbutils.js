93b72d5722b4f805961d8834c5211812a2a9700d
creesch <creesch.r@gmail.com>
2014-12-22 18:59:01 +0100

    What's this? A release name? could it?!

--- tbutils.js
+++ tbutils.js
@@ -36,7 +36,7 @@ function initwrapper() {
     // Public variables
     TBUtils.toolboxVersion = '3.0.0' + ((betaRelease) ? ' (beta)' : '');
     TBUtils.shortVersion = 300; //don't forget to change this one!  This is used for the 'new version' notification.
-    TBUtils.releaseName = '???';
+    TBUtils.releaseName = 'Illuminati Ibis';
     TBUtils.configSchema = 1;
     TBUtils.notesSchema = 4;
     TBUtils.minNotesSchema = 0;

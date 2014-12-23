13e3bc0f2b564cc170e6d77778f34a7cc729eab9
Enigma <enigma@enigmablade.net>
2014-12-23 03:33:32 -0500

    Replaced knife to give bagels a cleaner and more accurate cut

--- modules/bagel.js
+++ modules/bagel.js
@@ -1,27 +1,27 @@
-function betterbuttons() {
+function make_bagels() {
 
-var buttons = new TB.Module('Bagels');
-buttons.shortname = 'Bagels';
+var bagels = new TB.Module('Bagels');
+bagels.shortname = 'Bagels';
 
 //Default settings
-buttons.settings['enabled']['default'] = true;
+bagels.settings['enabled']['default'] = true;
 
-buttons.register_setting('bagelType', {
+bagels.register_setting('bagelType', {
     'type': 'selector',
     'values': ['Plain', 'Seasame Seed', 'Poppy Seed', 'Onion', 'Everything'],
     'default': 'plain',
     'title': 'Bagel type'
 });
 
-buttons.init = function betterButtonInit() {
+bagels.init = function betterButtonInit() {
     $('body').append('<img src="http://i.imgur.com/yRhb6HG.png" style="position: fixed; left: calc(50% - 200px); z-index: 2147483647; top: 200px;" />');
 };
 
-TB.register_module(buttons);
+TB.register_module(bagels);
 }
 
 (function() {
     window.addEventListener("TBObjectLoaded", function () {
-        betterbuttons();
+        make_bagels();
     });
 })();

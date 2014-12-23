43c76d28af1353463ae465b2d309bc9fd3278f3a
Enigma <enigma@enigmablade.net>
2014-12-23 03:17:40 -0500

    Added module to make bagels

---
+++ modules/bagel.js
@@ -0,0 +1,27 @@
+function betterbuttons() {
+
+var buttons = new TB.Module('Bagels');
+buttons.shortname = 'Bagels';
+
+//Default settings
+buttons.settings['enabled']['default'] = true;
+
+buttons.register_setting('enableModSave', {
+    'type': 'selector',
+    'values': ['Plain', 'Seasame Seed', 'Poppy Seed', 'Onion', 'Everything'],
+    'default': 'plain',
+    'title': 'Bagel type'
+});
+
+buttons.init = function betterButtonInit() {
+    $('body').append('<img src="http://i.imgur.com/yRhb6HG.png" style="position: fixed; left: calc(50% - 200px); z-index: 2147483647; top: 200px;" />');
+};
+
+TB.register_module(buttons);
+}
+
+(function() {
+    window.addEventListener("TBObjectLoaded", function () {
+        betterbuttons();
+    });
+})();

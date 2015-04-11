toolbox-team/reddit-moderator-toolbox

commit 6c2ec3c78b1be282b098a631a2d66070f1ff4fcc

fixes #492

modules/modbutton.js
@@ -39,7 +39,7 @@ self.processThing = function (thing) {
         $(thing).addClass('mod-button');
 
         // Defer info gathering until button is clicked.
-        $(thing).find('.buttons > li:last').before('<li><a href="javascript:;" class="global-mod-button">' + self.buttonName + '</a></li>');
+        $(thing).find('.flat-list.buttons a[onClick="return reply(this)"]').before('<li><a href="javascript:;" class="global-mod-button">' + self.buttonName + '</a></li>');
     }
 };
 

commit 19fa757a0f7d9870c971a228a3ac112ea98859dc

fix #493
Fixes a bug introduced by 6c2ec3c78b1be282b098a631a2d66070f1ff4fcc

modules/modbutton.js
@@ -39,7 +39,13 @@ self.processThing = function (thing) {
         $(thing).addClass('mod-button');
 
         // Defer info gathering until button is clicked.
-        $(thing).find('.flat-list.buttons a[onClick="return reply(this)"]').before('<li><a href="javascript:;" class="global-mod-button">' + self.buttonName + '</a></li>');
+        // try to insert it to the left of 'reply'
+        var $insertionPoint = $(thing).find('.flat-list.buttons a[onClick="return reply(this)"]');
+        if ($insertionPoint.length == 0) {
+            // otherwise stick it to the left of the last button
+            $insertionPoint = $(thing).find('.buttons > li:last');
+        }
+        $insertionPoint.before('<li><a href="javascript:;" class="global-mod-button">' + self.buttonName + '</a></li>');
     }
 };
 
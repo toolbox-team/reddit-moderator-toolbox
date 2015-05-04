2f355dcf37eebe3e618b54055ae66537b6b4fb1d
Garethp <gareth.parker@gareth.hobsons.com.au>
2015-05-04 12:20:32 +1000

    Issue-#438
     - Rewrote Metric Module
     - Made it easier to have multiple metrics

--- modules/metricstab.js
+++ modules/metricstab.js
@@ -1,53 +1,73 @@
 function metricstab() {
-// @copyright  2013+, LowSociety, dakta
+	var self = new TB.Module('Metrics Tab');
+	self.shortname = 'Metrics';
 
-var self = new TB.Module('Metrics Tab');
-self.shortname = 'Metrics';
+	self.settings['enabled']['default'] = true;
 
-self.settings['enabled']['default'] = true;
+	self.getSectionFromUrl = function getSectionFromUrl(url) {
+	    var regex = new RegExp(/^(http|https):\/\/([a-z]+\.)?reddit\.com\/(user|r)\/([^\/]+)(\/|$)/g);
+	    var matches = regex.exec(url);
 
-self.getSectionFromUrl = function getSectionFromUrl(url) {
-    var regex = new RegExp(/^(http|https):\/\/([a-z]+\.)?reddit\.com\/(user|r)\/([^\/]+)(\/|$)/g);
-    var matches = regex.exec(url);
+	    if (matches != null) {
+	        return {section: matches[3], subSection: matches[4]};
+	    } else {
+	        return null;
+	    }
+	};
 
-    if (matches != null) {
-        return {section: matches[3], subSection: matches[4]};
-    } else {
-        return null;
-    }
-};
+	self.init = function() {
+	    var page = this.getSectionFromUrl(window.location.href);
 
-self.init = function() {
-    var page = this.getSectionFromUrl(window.location.href);
+	    if (page == null) {
+	        return false;
+	    }
 
-    if (page == null) {
-        return false;
-    }
+		var metrics = {
+			user: {
+				'SnoopSnoo': 'http://www.snoopsnoo.com/u/{subSection}',
+				'Observatory': 'http://0bservat0ry.com/reddit/u/{subSection}.html',
+				'Karmawhores' : 'http://www.karmawhores.net/user/{subSection}'
+			},
 
-    var header = document.getElementById("header-bottom-left");
-    var tabList = header.getElementsByTagName("ul")[0];
+			r: {
+				'MetaReddit': 'http://metareddit.com/r/{subSection}',
+				'RedditMetrics': 'http://redditmetrics.com/r/{subSection}'
+			}
+		};
+		
+	    var header = document.getElementById("header-bottom-left");
+	    var tabList = header.getElementsByTagName("ul")[0];
 
-    if (tabList == null) {
-        return false;
-    }
+	    if (tabList == null) {
+	        return false;
+	    }
 
-    var listItem = document.createElement("li");
-    var link = document.createElement("a");
+		var $tabList = $(tabList);
+		$tabList.css('overflow', 'visible');
 
-    if (page.section === 'user') {
-        link.href = "http://karmawhores.net/" + page.section + "/" + page.subSection;
-        link.innerHTML = "karma stats";
-    } else {
-        link.href = "http://redditmetrics.com/" + page.section + "/" + page.subSection;
-        link.innerHTML = "reddit metrics";
-    }
+		var $listItem = $("<li class='dropdown tb-metrics' style=''><a href='#'>Metrics</a></li>");
+		$listItem.css({
+			overflow: 'visible',
+			position: 'relative'
+		});
 
-    link.target = "_blank";
-    listItem.appendChild(link);
-    tabList.appendChild(listItem);
-};
+		$(tabList).append($listItem);
 
-TB.register_module(self);
+		var links = metrics[page.section];
+		for(var i in links)
+		{
+			var url = links[i];
+			url = url.replace(/\{subSection\}/g, page.subSection);
+			$tabList.append("<li style='display: none;' class='hidden metricTab'><a href='" + url + "'>" + i + "</a></li>");
+		}
+
+		$listItem.on('click', function()
+		{
+			$tabList.find('.metricTab').toggle();
+		});
+	};
+
+	TB.register_module(self);
 }
 
 (function () {

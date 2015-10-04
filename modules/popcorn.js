3d224c9bed5f63ea88bdc5c30f909570fb380b10
LiterallyWTH <Joseph@Swagintosh.local>
2015-10-03 19:15:15 -0500

    Made highlighting optional via button.

--- modules/popcorn.js
+++ modules/popcorn.js
@@ -5,10 +5,10 @@ function popcorn() {
 //Default settings
 self.settings['enabled']['default'] = false;
 
-self.register_setting('highlight', {
+self.register_setting('highlightAuto', {
     'type': 'boolean',
-    'default': true,
-    'title': 'Highlight downvoted/controversial comments'
+    'default': false,
+    'title': 'Highlight comments automatically'
 });
 
 self.register_setting('negHighlightThreshold', {
@@ -35,16 +35,14 @@ self.pending = [];
 self.init = function() {
     var neg_thresh = self.setting('negHighlightThreshold'),
         expand = self.setting('expandOnLoad'),
-        highlight = self.setting('highlight'),
+        auto = self.setting('highlightAuto'),
         sortOnMoreChildren = self.setting('sortOnMoreChildren'),
         $body = $('body'),
+        $buttons = $('<div id="tb-popcorn-buttons">'),
+        $init = $('<button id="tb-popcorn-init" class="tb-action-button">Trouble-seeker</button>').click(init),
         $sitetable;
 
     // if(!TBUtils.isMod) return;
-
-    if(highlight) $body.addClass('tb-popcorn');
-
-    if(!TBUtils.isCommentsPage) return;
 
     if($body.hasClass('listing-page')){
         $sitetable = $('.content > .sitetable');
@@ -52,23 +50,38 @@ self.init = function() {
         $sitetable = $('.commentarea > .sitetable');
     }
 
-    $sitetable.before(
-        $('<div id="tb-popcorn-buttons">')
-            .append($('<button style="margin-right:10px;" id="tb-popcorn-popcorn" class="tb-action-button">Popcorn!</button>').click(sortChildren))
-            .append($('<button class="tb-action-button" id="tb-popcorn-salt">Salt Please!</button>').click(collapseNonDrama))
-    );
+    $sitetable.before($buttons);
 
-    $('.commentarea').on('click', '.morecomments', function(){
-        if(sortOnMoreChildren) self.pending.push(sortMe.bind($(this).closest('.sitetable')));
-    })
-
-    window.addEventListener("TBNewThings", function () {
+    if(auto){
         init();
-    });
-
-    init();
+    } else {
+        $buttons.append($init);
+    }
 
     function init(){
+
+        $init.remove();
+
+        $body.addClass('tb-popcorn');
+
+        if(!TBUtils.isCommentsPage) return;
+
+        $buttons.append($('<button id="tb-popcorn-sort" class="tb-action-button">Sort</button>').click(sortChildren))
+                .append($('<button class="tb-action-button" id="tb-popcorn-collapse">Collapse</button>').click(collapseNonDrama));
+
+        $('.commentarea').on('click', '.morecomments', function(){
+            if(sortOnMoreChildren) self.pending.push(sortMe.bind($(this).closest('.sitetable')));
+        });
+
+        window.addEventListener("TBNewThings", function () {
+            run();
+        });
+        
+        run();
+    }
+
+
+    function run(){
 
         highlightComments();
 

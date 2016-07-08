56e28d2dc7d1856cc781ec219f8cab7f6fe140ac
Enigma <theenigmablade@gmail.com>
2016-07-08 18:20:39 -0400

    Update gulpfile to select the appropriate manifest based on browser. The Firefox manifest is currently unused because we're not building a web extensions version yet.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 var exec = require('child_process').exec;
 var argv = require('yargs').argv;
+var Stream = require('stream');
+const Path = require('path');
 
 var src_dir = "extension";
 var dest_dir = "build";
@@ -9,6 +11,23 @@ var dest_dir = "build";
 // Used when the --post parameter is given to gulp in order to push the xpi to firefox.
 var postUrl = "http://localhost:8888/";
 
+// Renames the manifest for the specified browser (ex. manifest-chrome.json)
+// to the appropriate "manifest.json" within the file stream.
+function manifestFor(browser) {
+    var stream = new Stream.Transform({objectMode: true});
+    stream._transform = function (originalFile, unused, callback) {
+        var file = originalFile.clone({contents: false});
+        if (Path.basename(file.relative) == "manifest-"+browser+".json") {
+            var dir = Path.dirname(file.relative);
+            file.path = Path.join(file.base, dir, "manifest.json");
+        }
+        callback(null, file);
+     };
+    
+    return stream;
+}
+
+// Tasks
 gulp.task('zip', function() {
     console.log(process.cwd());
 
@@ -20,6 +39,7 @@ gulp.task('zip', function() {
         '!'+src_dir+'/data/background/',
         '!'+src_dir+'/data/Icon.png'
     ])
+        .pipe(manifestFor('chrome')
         .pipe(zip('chrome-moderator-toolbox.zip'))
         .pipe(gulp.dest(dest_dir));
 });

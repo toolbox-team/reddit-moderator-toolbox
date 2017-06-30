21ad02e4384addd3e8a91fe8be09327ffcf5e6d5
creesch <creesch.r@gmail.com>
2017-06-30 22:57:13 +0200

    Accidentally commented of something that shouldn't.

--- extension/data/tbui.js
+++ extension/data/tbui.js
@@ -620,7 +620,7 @@
                 </style>`);
                 var $body = $('body');
                 if (location.host === 'mod.reddit.com') {
-                    //$body.append('<div id="tb-loading-stuff"><span class="tb-loading-content"><img src="https://creesch.github.io/reddit-moderator-toolbox/hosted_images/long_load_spinner.gif" alt="loading"> <span class="tb-loading-text">' + TBUtils.RandomFeedback + '</span></span></div>');
+                    $body.append('<div id="tb-loading-stuff"><span class="tb-loading-content"><img src="https://creesch.github.io/reddit-moderator-toolbox/hosted_images/long_load_spinner.gif" alt="loading"> <span class="tb-loading-text">' + TBUtils.RandomFeedback + '</span></span></div>');
                     $body.append('<div id="tb-loading"></div>');
                 } else {
                     $('.footer-parent').append('<div id="tb-loading-stuff"><span class="tb-loading-content"><img src="https://creesch.github.io/reddit-moderator-toolbox/hosted_images/long_load_spinner.gif" alt="loading"> <span class="tb-loading-text">' + TBUtils.RandomFeedback + '</span></span></div>');

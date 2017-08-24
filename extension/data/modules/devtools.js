cef37811c7451a50aec2596a32ba60255c74220c
Erik Vosseberg <e.vosseberg@gmail.com>
2017-08-24 11:37:42 +0200

    devtools, replace var for const.

--- extension/data/modules/devtools.js
+++ extension/data/modules/devtools.js
@@ -1,7 +1,7 @@
 function devtools() {
 // Developer Tools, for managing /r/toolbox/w/tbnotes (etc.)
 
-    var self = new TB.Module('Developer Tools');
+    const self = new TB.Module('Developer Tools');
     self.settings['enabled']['default'] = false;
 
     self.setting('betamode', false);

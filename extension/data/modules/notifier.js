443da4f3ff0827708a248bc9bc7a0bd78c058185
creesch <creesch@users.noreply.github.com>
2017-12-01 23:19:34 +0100

    Make notifier work again.

--- extension/data/modules/notifier.js
+++ extension/data/modules/notifier.js
@@ -530,7 +530,7 @@ function notifiermod() {
 
             // We still want counts updated, just no notifications shown.
             // That's why we do this here.
-            if (wwwNotifications && TB.utils.domain !== 'www') {  //It's intentional that we don't also check for mod.reddit, here.  That would still cause dup messages.
+            if (wwwNotifications && TB.utils.domain !== 'www' && TB.utils.domain !== 'alpha') {  //It's intentional that we don't also check for mod.reddit, here.  That would still cause dup messages.
                 self.log("non-www domain; don't show notifications");
                 updateMessagesCount(unreadMessageCount);
                 updateModqueueCount(modqueueCount);

6715cd2e02f79f6e7e311ca5276e3604a2414a57
creesch <creesch@users.noreply.github.com>
2017-12-09 20:22:32 +0100

    Flyingsnoo, now with extra requestAnimationFrame!

--- extension/data/modules/flyingsnoo.js
+++ extension/data/modules/flyingsnoo.js
@@ -11,70 +11,92 @@ function flyingsnoo() {
     self.settings['enabled']['hidden'] = true;  // it's an easter egg.
 
     self.init = function () {
-        if (!TB.utils.isUserPage) return;
+        //if (!TB.utils.isUserPage) return;
+        const $snooFooter = $('<div id="tb-snoo-footer"></div>').appendTo('#tb-footer-block');
+        $snooFooter.css({
+            'background-image': `url(${chrome.runtime.getURL('data/images/balloon.png')})`,
+            'background-repeat': 'no-repeat'
 
-        $('.profile-page .footer-parent').click(function () {
+        });
+        $snooFooter.click(function () {
 
-            var width = 87,
+            let width = 87,
                 height = 145;
 
             // unlock achievement
             TB.utils.sendEvent(TB.utils.events.TB_FLY_SNOO);
 
-            var floater = $('<div></div>').css({
+            let floater = $('<div></div>').css({
                 height: `${height}px`,
                 width: `${width}px`,
-                background: 'url(//c.thumbs.redditmedia.com/7U0yjjycFy9MC5ht.png)',
+                background: `url(${chrome.runtime.getURL('data/images/snoo_up.png')})`,
                 position: 'absolute',
                 top: `${$(this).offset().top}px`,
                 left: `${($(window).width() * 0.49) - (width / 2)}px`,
                 zIndex: 999
             }).appendTo('body');
 
-            var documentHeight = $(document).height(),
+            let documentHeight = $(document).height(),
                 documentWidth = $(document).width(),
                 iterations = 0,
                 wind = 0,
                 oldTop = floater.position().top,
                 oldLeft = floater.position().left;
 
-            var interval = null;
+            let keepFlying = false;
 
-            var startInterval = function () {
-                interval = window.setInterval(function () {
-                    var newTop = Math.max(0, (oldTop - ((documentHeight) * 0.001)));
 
-                    if (iterations % 50 == 0) {
-                        wind = ((Math.random() * 200) - 100) * 0.02;
-                    }
+            function startFlying() {
+                const newTop = Math.max(0, (oldTop - ((documentHeight) * 0.001)));
 
-                    var newLeft = Math.min(documentWidth - width, Math.max(0, oldLeft + wind));
+                if (iterations % 50 == 0) {
+                    wind = ((Math.random() * 200) - 100) * 0.02;
+                }
 
+                const newLeft = Math.min(documentWidth - width, Math.max(0, oldLeft + wind));
+
+                floater.css({
+                    top: `${newTop}px`,
+                    left: `${newLeft}px`
+                });
+
+                iterations++;
+                if (newTop > 0 && keepFlying) {
+                    requestAnimationFrame(startFlying);
+                }
+                oldTop = newTop;
+                oldLeft = newLeft;
+
+            }
+
+            function killSnoo() {
+                let newTop = oldTop + Math.max(50, Math.min(10, oldTop * 0.05));
+                floater.css({
+                    'top': `${newTop}px`
+                });
+                oldTop = newTop;
+                if (oldTop + height >= documentHeight) {
                     floater.css({
-                        top: `${newTop}px`,
-                        left: `${newLeft}px`
+                        'top': `${documentHeight - height - 40}px`
                     });
+                    floater.css('background', `url(${chrome.runtime.getURL('data/images/snoo_splat.png')})`);
 
-                    iterations++;
-                    if (newTop <= 0) {
-                        window.clearInterval(interval);
-                        interval = null;
-                    }
-                    oldTop = newTop;
-                    oldLeft = newLeft;
-                }, 50);
-            };
+                    // unlock achievement
+                    TB.utils.sendEvent(TB.utils.events.TB_KILL_SNOO);
+                } else {
+                    requestAnimationFrame(killSnoo);
+                }
+            }
 
             floater.mousedown(function (e) {
-                if (interval != null) {
-                    window.clearInterval(interval);
-                    interval = null;
+                if (keepFlying) {
+                    keepFlying = false;
                 }
                 floater.data('offsetX', e.offsetX);
                 floater.data('offsetY', e.offsetY);
 
-                var dragEvent = function (e) {
-                    var offsetX = floater.data('offsetX') || 0,
+                const dragEvent = function (e) {
+                    let offsetX = floater.data('offsetX') || 0,
                         offsetY = floater.data('offsetY') || 0;
                     oldLeft = (e.pageX - offsetX);
                     oldTop = (e.pageY - offsetY);
@@ -84,44 +106,30 @@ function flyingsnoo() {
                     });
                 };
 
-                var releaseEvent = function () {
-                    if (interval == null)
-                        startInterval();
-                    $(document).unbind('mousemove', dragEvent);
-                    $(document).unbind('mouseup', releaseEvent);
+                const releaseEvent = function () {
+                    if (!keepFlying) {
+                        keepFlying = true;
+                        startFlying();
+                        $(document).unbind('mousemove', dragEvent);
+                        $(document).unbind('mouseup', releaseEvent);
+                    }
+
                 };
 
                 $(document).bind('mousemove', dragEvent).bind('mouseup', releaseEvent);
 
             }).dblclick(function () {
-                if (interval != null) {
-                    window.clearInterval(interval);
-                    interval = null;
+                if (keepFlying) {
+                    keepFlying = false;
                 }
                 $(this).unbind('mousedown');
-                $(this).css('background', 'url(//a.thumbs.redditmedia.com/P0tVLpH52smMNXPN.png)');
-                interval = window.setInterval(function () {
-                    var newTop = oldTop + Math.max(50, Math.min(10, oldTop * 0.05));
-                    floater.css({
-                        'top': `${newTop}px`
-                    });
-                    oldTop = newTop;
-                    if (oldTop + height >= documentHeight) {
-                        window.clearInterval(interval);
-                        interval = null;
-                        floater.css({
-                            'top': `${documentHeight - height}px`
-                        });
-                        floater.css('background', 'url(//d.thumbs.redditmedia.com/tIFOjQQ5pPahJKP-.png)');
-
-                        // unlock achievement
-                        TB.utils.sendEvent(TB.utils.events.TB_KILL_SNOO);
-                    }
-                }, 50);
+                $(this).css('background', `url(${chrome.runtime.getURL('data/images/snoo_uh_oh.png')})`);
+                killSnoo();
             });
 
             $(this).css('background', 'transparent');
-            startInterval();
+            keepFlying = true;
+            startFlying();
         });
     };
 

3cf42e7b28fcc3adc76efb043d04bc2fe235c939
Eric <spytec13@gmail.com>
2019-10-06 11:24:31 +0200

    fix(Macros): Position correctly in new modmail

--- extension/data/modules/macros.js
+++ extension/data/modules/macros.js
@@ -159,7 +159,7 @@ function modmacros () {
             // if we're a mod, add macros to top level reply button.
                 if (success && config.length > 0) {
                     const macroButtonHtml = `<select class="tb-macro-select tb-action-button" data-subreddit="${info.subreddit}"><option value=${MACROS}>macros</option></select>`;
-                    $body.find('.ThreadViewerReplyForm__replyOptions').before(`<div class="tb-usertext-buttons tb-macro-newmm">${macroButtonHtml}</div>`);
+                    $body.find('.ThreadViewerReplyForm__replyOptions').after(`<div class="tb-usertext-buttons tb-macro-newmm">${macroButtonHtml}</div>`);
 
                     populateSelect('.tb-macro-select', info.subreddit, config);
                 }

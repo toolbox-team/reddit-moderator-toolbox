10c0b37d514f2f75cf958337bfcf68cd66493021
Eric <SpyTec@users.noreply.github.com>
2019-10-15 21:22:50 +0200

    fix(About): Removed "literally" title from SpyTec

--- extension/data/tbmodule.js
+++ extension/data/tbmodule.js
@@ -239,7 +239,7 @@ function tbmodule () {
                             <td><a href="https://www.reddit.com/user/amici_ursi">/u/amici_ursi</a></td>
                             <td><a href="https://www.reddit.com/user/geo1088">/u/geo1088</a></td>
                         </tr><tr>
-                            <td><a href="https://www.reddit.com/user/SpyTec13" title="Literally">/u/SpyTec13</a></td>
+                            <td><a href="https://www.reddit.com/user/SpyTec13">/u/SpyTec13</a></td>
                             <td></td>
                             <td></td>
                         </tr>

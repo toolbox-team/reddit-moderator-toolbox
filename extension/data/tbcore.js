c1f63ea5371c3c39c8881fa06f0ee79fa00a17f6
Newcool1230 <newcool.1230@yahoo.ca>
2019-10-20 16:37:30 -0500

    Update tbcore.js
    Added new text

--- extension/data/tbcore.js
+++ extension/data/tbcore.js
@@ -98,7 +98,18 @@ function initwrapper ({userDetails, newModSubs, cacheDetails}) {
                   'Releasing raptors',
                   'Booting robot uprising',
                   'I need to tell you something critically important! I am sure I will remember in a moment...',
-                  "/u/dakta ran out for a pack of smokes... BUT HE PROMISED HE'D BE RIGHT BACK"];
+                  "/u/dakta ran out for a pack of smokes... BUT HE PROMISED HE'D BE RIGHT BACK",
+                  'Yeeting your feedback to the trashcan.',
+                  'Here listen to some music while you wait, https://youtu.be/dQw4w9WgXcQ',
+                  'Me? No. I\'m no docter but it looks like you have a broken toe.',
+                  'Boo! Scared yeh didn\'t I?',
+                  'If it looks like a goat, sounds like a goat, acts like a goat. Then its probably not a duck.',
+                  'The last thing you want to do is needing to pee before a battle.',
+                  'When in doubt, check the lost and found.',
+                  'Can you check under the table? I\'ve seem to have lost my pants.',
+                  'Don\'t rustle my jimmies.',
+                  'Hello and, again, welcome to the Toolbox Science computer-aided enrichment center.',
+                  'Did you know... you are naked under your clothes?'];
 
         let gettingModSubs = false;
         // Public variables

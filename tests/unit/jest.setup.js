8920daa7197325f7620be636c1890f6aa98ee962
Eric <spytec13@gmail.com>
2019-10-22 00:18:21 +0200

    test: Initial proper support test

--- tests/unit/jest.setup.js
+++ tests/unit/jest.setup.js
@@ -1,4 +1,5 @@
 'use strict';
+window.$ = require('../../extension/data/libs/jquery-3.4.1');
 
 // Mock module and register_module
 window.TB = {
@@ -7,10 +8,18 @@ window.TB = {
         shortname: 'support',
         settings: {
             enabled: {
-                default: false,
-                hidden: false,
+                default: null,
+                hidden: null,
             },
         },
     })),
     register_module: jest.fn(),
+};
+const debugInformationField = jest.fn().mockReturnValue('debugInfoField');
+window.TBCore = {
+    debugInformation: jest.fn().mockReturnValue(debugInformationField),
+};
+
+window.TBHelpers = {
+    template: jest.fn().mockReturnValue('test response'),
 };
--- tests/unit/modules/support.test.js
+++ tests/unit/modules/support.test.js
@@ -2,10 +2,57 @@
 require('../../../extension/data/modules/support');
 
 describe('support.js', () => {
-    it('calls TB.Module', () => {
-        // Dispatch an event to call the function
+    beforeEach(() => {
+        window.TB.Module.mockClear();
+    });
+    it('calls TB.Module with "Support Module"', () => {
         const event = new CustomEvent('TBModuleLoaded');
         window.dispatchEvent(event);
         expect(window.TB.Module).toHaveBeenCalledWith('Support Module');
+    });
+    it('sets name to "support"', () => {
+        const event = new CustomEvent('TBModuleLoaded');
+        window.dispatchEvent(event);
+        expect(window.TB.Module.mock.results[0].value.shortname).toEqual('support');
+    });
+    it('sets name to enable.default to true', () => {
+        const event = new CustomEvent('TBModuleLoaded');
+        window.dispatchEvent(event);
+        expect(window.TB.Module.mock.results[0].value.settings.enabled.default).toBeTruthy();
+    });
+    it('sets name to enabled.hidden to true', () => {
+        const event = new CustomEvent('TBModuleLoaded');
+        window.dispatchEvent(event);
+        expect(window.TB.Module.mock.results[0].value.settings.enabled.hidden).toBeTruthy();
+    });
+    describe('init()', () => {
+        beforeEach(() => {
+            window.TBCore.debugInformation.mockClear;
+        });
+        it('calls TBCore.debugInformation()', () => {
+            const event = new CustomEvent('TBModuleLoaded');
+            window.dispatchEvent(event);
+            window.TB.Module.mock.results[0].value.init();
+            expect(window.TBCore.debugInformation).toHaveBeenCalled();
+        });
+        it('calls updates textArea with debug info on r/toolbox link submit', () => {
+            window.history.pushState({}, 'Page Title', '/r/toolbox/submit');
+
+            const textArea = 'test value';
+            document.body.innerHTML =
+                `<div class="submit content usertext-edit md-container">  
+                    <textarea>${textArea}</textarea>
+                    <button class="btn" name="submit" />
+                </div>`;
+
+            const event = new CustomEvent('TBModuleLoaded');
+            window.dispatchEvent(event);
+            window.TB.Module.mock.results[0].value.init();
+
+            $('.submit.content .btn[name="submit"]').click();
+
+            const textAreaResult = textArea + window.TBHelpers.template.mock.results[0].value;
+            expect($('.usertext-edit.md-container textarea').val()).toEqual(textAreaResult);
+        });
     });
 });

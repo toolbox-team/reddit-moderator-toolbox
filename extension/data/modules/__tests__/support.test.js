0aeae51d534503d20d7e4bf10134c696f1a13357
Eric <spytec13@gmail.com>
2019-10-25 15:20:12 +0200

    Added first unit test
    # Conflicts:
    #	package.json

---
+++ extension/data/modules/__tests__/support.test.js
@@ -0,0 +1,23 @@
+'use strict';
+require('../support');
+
+describe('support.js', () => {
+    it('calls TB.Module', () => {
+        window.TB = {
+            Module: jest.fn(),
+            register_module: jest.fn(),
+        };
+        window.TB.Module.mockImplementation(() => ({
+            shortname: 'support',
+            settings: {
+                enabled: {
+                    default: false,
+                    hidden: false,
+                },
+            },
+        }));
+        const event = new CustomEvent('TBModuleLoaded');
+        window.dispatchEvent(event);
+        expect(window.TB.Module).toHaveBeenCalled();
+    });
+});

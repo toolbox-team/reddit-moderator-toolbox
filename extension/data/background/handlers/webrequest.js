5aa9bcbd689efb53a6fbc1b9f720db76b89e3348
Geo1088 <georgej1088@gmail.com>
2020-02-09 00:37:53 -0500

    Convert getOAuthTokens to async
    Only wrap the callback-only parts of the function in `new Promise()` for consistency

--- extension/data/background/handlers/webrequest.js
+++ extension/data/background/handlers/webrequest.js
@@ -6,50 +6,50 @@
  * @returns {Promise<Object>} An object with properties `accessToken`,
  * `refreshToken`, `scope`, and some others
  */
-function getOAuthTokens (tries = 1) {
-    return new Promise(async (resolve, reject) => {
-        // This function will fetch the cookie and if there is no cookie attempt to create one by visiting modmail.
-        // http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
+async function getOAuthTokens (tries = 1) {
+    // This function will fetch the cookie and if there is no cookie attempt to create one by visiting modmail.
+    // http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
 
-        const cookieInfo = {url: 'https://mod.reddit.com', name: 'token'};
-        let rawCookie;
-        try {
-            rawCookie = await browser.cookies.get(cookieInfo);
-        } catch (error) {
-            // If first-party isolation is enabled in Firefox, `cookies.get`
-            // throws when not provided a `firstPartyDomain`, so we try again
-            // passing the first-party domain for the cookie we're looking for.
-            cookieInfo.firstPartyDomain = 'reddit.com';
-            rawCookie = await browser.cookies.get(cookieInfo);
-        }
-        // If we do get a rawcookie we first want to make sure it is still valid.
-        let expired = false;
-        if (rawCookie) {
-            const cookieExpiration = rawCookie.expirationDate * 1000;
-            const timeNow = Date.now();
-            expired = timeNow > cookieExpiration;
-            console.log('Found cookie expired:', expired);
-        }
-        // If no cookie is returned it is probably expired and we will need to generate a new one.
-        // Instead of trying to do the oauth refresh thing ourselves we just do a GET request for modmail.
-        // We try this three times, if we don't have a cookie after that the user clearly isn't logged in.
-        if ((!rawCookie || expired) && tries < 3) {
+    const cookieInfo = {url: 'https://mod.reddit.com', name: 'token'};
+    let rawCookie;
+    try {
+        rawCookie = await browser.cookies.get(cookieInfo);
+    } catch (error) {
+        // If first-party isolation is enabled in Firefox, `cookies.get`
+        // throws when not provided a `firstPartyDomain`, so we try again
+        // passing the first-party domain for the cookie we're looking for.
+        cookieInfo.firstPartyDomain = 'reddit.com';
+        rawCookie = await browser.cookies.get(cookieInfo);
+    }
+    // If we do get a rawcookie we first want to make sure it is still valid.
+    let expired = false;
+    if (rawCookie) {
+        const cookieExpiration = rawCookie.expirationDate * 1000;
+        const timeNow = Date.now();
+        expired = timeNow > cookieExpiration;
+        console.log('Found cookie expired:', expired);
+    }
+    // If no cookie is returned it is probably expired and we will need to generate a new one.
+    // Instead of trying to do the oauth refresh thing ourselves we just do a GET request for modmail.
+    // We try this three times, if we don't have a cookie after that the user clearly isn't logged in.
+    if ((!rawCookie || expired) && tries < 3) {
+        return new Promise(resolve => {
             $.get('https://mod.reddit.com/mail/all').done(data => {
                 console.log('data:', data);
                 // Ok we have the data, let's give this a second attempt.
                 getOAuthTokens(tries + 1).then(resolve);
             });
-        } else if ((!rawCookie || expired) && tries > 2) {
-            reject(new Error('user not logged into new modmail'));
-        } else {
-            // The cookie we grab has a base64 encoded string with data. Sometimes is invalid data at the end.
-            // This RegExp should take care of that.
-            const invalidChar = new RegExp('[^A-Za-z0-9+/].*?$');
-            const base64Cookie = rawCookie.value.replace(invalidChar, '');
-            const tokenData = atob(base64Cookie);
-            resolve(JSON.parse(tokenData));
-        }
-    });
+        });
+    } else if ((!rawCookie || expired) && tries > 2) {
+        throw new Error('user not logged into new modmail');
+    } else {
+        // The cookie we grab has a base64 encoded string with data. Sometimes is invalid data at the end.
+        // This RegExp should take care of that.
+        const invalidChar = new RegExp('[^A-Za-z0-9+/].*?$');
+        const base64Cookie = rawCookie.value.replace(invalidChar, '');
+        const tokenData = atob(base64Cookie);
+        return JSON.parse(tokenData);
+    }
 }
 
 /**

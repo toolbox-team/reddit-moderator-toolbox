9775629a3991e1aee39fae34bcda46636f16ef06
Geo <georgej1088@gmail.com>
2020-07-28 19:45:38 -0400

    Generate url_* tokens if not present in API info

--- extension/data/modules/removalreasons.js
+++ extension/data/modules/removalreasons.js
@@ -229,8 +229,8 @@ function removalreasons () {
                     domain: info.domain,
                     body: info.body,
                     raw_body: info.raw_body,
-                    uri_body: info.uri_body,
-                    uri_title: info.uri_title,
+                    uri_body: info.uri_body || encodeURIComponent(info.uri_body),
+                    uri_title: info.uri_title || encodeURIComponent(info.title),
                     reasons: [],
                     get reasons_posts () {
                         return this.reasons.filter(r => r.removePosts || r.removePosts === undefined);

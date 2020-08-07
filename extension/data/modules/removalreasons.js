4acc0dea3f09a9f1964603cdfbece519cb7ed8c9
Geo <georgej1088@gmail.com>
2020-08-07 07:05:29 -0400

    recognize toolbox-generated comments as comments for RRs (#335)
    * recognize toolbox-generated comments as comments for RRs
    
    * let tb-generated stuff trigger rrs in new reddit

--- extension/data/modules/removalreasons.js
+++ extension/data/modules/removalreasons.js
@@ -191,10 +191,11 @@ function removalreasons () {
                 return;
             }
 
-            if (TBCore.isOldReddit) {
+            // For now, removals on Toolbox-generated posts/comments work the same way as on old Reddit (without jsAPI)
+            if (TBCore.isOldReddit || $button.is('.tb-submission-button-remove, .tb-comment-button-remove')) {
                 const $yes = $button.find('.yes')[0],
                       $thing = $button.closest('.thing, .tb-thing');
-                isComment = $thing.hasClass('comment') || $thing.hasClass('was-comment');
+                isComment = $thing.hasClass('comment') || $thing.hasClass('was-comment') || $thing.hasClass('tb-comment');
 
                 if ($yes) {
                     $yes.click();

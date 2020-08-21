0a053f0a1fd56b6553cc84f13a1dcd65b338ecb7
creesch <creesch@users.noreply.github.com>
2020-08-21 10:20:13 +0200

    Convert queue object to map

--- extension/data/background/handlers/modqueue.js
+++ extension/data/background/handlers/modqueue.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Object containing the queue cached data per subreddit
-const queueCacheObject = {};
+const queueCache = new Map();
 
 /**
  * Returns if a given thing is contained in the cache for the given subreddit.
@@ -11,8 +11,9 @@ const queueCacheObject = {};
  */
 function thingFound (thingName, subreddit) {
     let thingFound = false;
-    if (Object.prototype.hasOwnProperty.call(queueCacheObject, subreddit) && queueCacheObject[subreddit].things.includes(thingName)) {
-        thingFound = true;
+    if (queueCache.has(subreddit)) {
+        const subredditQueueCache = queueCache.get(subreddit);
+        thingFound = subredditQueueCache.things.includes(thingName);
     }
     return thingFound;
 }
@@ -23,57 +24,58 @@ messageHandlers.set('tb-modqueue', request => {
         // Check if we need to fetch data.
         let lastRefresh = 0;
         let refreshActive = false;
-        if (Object.prototype.hasOwnProperty.call(queueCacheObject, subreddit)) {
-            lastRefresh = queueCacheObject[subreddit].lastRefresh;
-            refreshActive = queueCacheObject[subreddit].refreshActive;
+        let subredditQueueCache;
+        if (queueCache.has(subreddit)) {
+            subredditQueueCache = queueCache.get(subreddit);
+            lastRefresh = subredditQueueCache.lastRefresh;
+            refreshActive = subredditQueueCache.refreshActive;
         }
 
-        // The thing timestamp is bigger than the last refresh or cache isn't fresh anymore.
-        if (thingTimestamp * 1000 > lastRefresh || Date.now() - lastRefresh > 1000 * 30) {
-            // To reduce api calls we don't do a new one if one is already running for this subreddit.
-            // Instead we wait for the finished event and then reference the cache object.
-            if (refreshActive) {
-                window.addEventListener(`freshCache-${subreddit}`, () => {
-                    resolve(thingFound(thingName, subreddit));
-                }, {
-                    once: true,
-                });
-            } else {
-                if (Object.prototype.hasOwnProperty.call(queueCacheObject, subreddit)) {
-                    queueCacheObject[subreddit].refreshActive = true;
-                } else {
-                    queueCacheObject[subreddit] = {
-                        refreshActive: true,
-                        lastRefresh: 0,
-                        things: [],
-                    };
-                }
+        // To reduce api calls we don't do a new one if one is already running for this subreddit.
+        // Instead we wait for the finished event and then reference the cache object.
+        if (refreshActive) {
+            window.addEventListener(`freshCache-${subreddit}`, () => {
+                resolve(thingFound(thingName, subreddit));
+            }, {
+                once: true,
+            });
 
-                makeRequest({
-                    method: 'GET',
-                    endpoint: `/r/${subreddit}/about/modqueue.json`,
-                    query: {
-                        limit: 100,
-                    },
-                    okOnly: true,
-                })
-                    .then(response => response.json())
-                    .then(updatedQueue => {
-                        const nowRefresh = Date.now();
-                        queueCacheObject[subreddit] = {
-                            lastRefresh: nowRefresh,
-                            things: updatedQueue.data.children.map(thing => thing.data.name),
-                            refreshActive: false,
-                        };
-                        window.dispatchEvent(new CustomEvent(`freshCache-${subreddit}`));
-                        resolve(thingFound(thingName, subreddit));
-                    }).catch(error => {
-                        console.error('getting modqeueue error: ', error);
-                        // Probably reddit errors, could build in a retry method but that seems overkill for now.
-                        // Resolving with whatever is in cache.
-                        resolve(thingFound(thingName, subreddit));
-                    });
+        // The thing timestamp is bigger than the last refresh or cache isn't fresh anymore.
+        } else if (thingTimestamp * 1000 > lastRefresh || Date.now() - lastRefresh > 1000 * 30) {
+            if (subredditQueueCache) {
+                subredditQueueCache.refreshActive = true;
+            } else {
+                queueCache.set(subreddit, {
+                    refreshActive: true,
+                    lastRefresh: 0,
+                    things: [],
+                });
             }
+            makeRequest({
+                method: 'GET',
+                endpoint: `/r/${subreddit}/about/modqueue.json`,
+                query: {
+                    limit: 100,
+                },
+                okOnly: true,
+            })
+                .then(response => response.json())
+                .then(updatedQueue => {
+                    const nowRefresh = Date.now();
+                    queueCache.set(subreddit, {
+                        lastRefresh: nowRefresh,
+                        things: updatedQueue.data.children.map(thing => thing.data.name),
+                        refreshActive: false,
+                    });
+                    window.dispatchEvent(new CustomEvent(`freshCache-${subreddit}`));
+                    resolve(thingFound(thingName, subreddit));
+                }).catch(error => {
+                    console.error('getting modqeueue error: ', error);
+                    // Probably reddit errors, could build in a retry method but that seems overkill for now.
+                    // Resolving with whatever is in cache.
+                    resolve(thingFound(thingName, subreddit));
+                });
+
         } else {
             resolve(thingFound(thingName, subreddit));
         }

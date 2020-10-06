47893926bd03074a6bf8ec74e7bc1e564012d571
Lars <larsjohnsen@gmail.com>
2020-10-06 20:56:52 +0200

    Catch error when notifying tab where Toolbox isn't active

--- extension/data/background/handlers/url_changed.js
+++ extension/data/background/handlers/url_changed.js
@@ -4,6 +4,12 @@
 // This is triggered in the background as there's no native event which is emitted when `history#pushState` modifies the URL.
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.url) {
-        browser.tabs.sendMessage(tab.id, {action: 'tb-url-changed'});
+        browser.tabs.sendMessage(tab.id, {action: 'tb-url-changed'})
+            .catch(error => {
+                // Receiving end errors are not really relevant to us and happen a lot for iframes and such where toolbox isn't active.
+                if (error.message !== 'Could not establish connection. Receiving end does not exist.') {
+                    console.warn('tb-url-changed: ', error.message, error);
+                }
+            });
     }
 });

toolbox-team/reddit-moderator-toolbox

commit 48212c77e4d67bb7efa72b8a54da1968b0069901

NewModMailPro: Fix message sending errors on FF (#389)
Without including `cancelable: false`, Firefox will invoke the normal form submission logic for the form. Since React manages the content of the fields, it ends up submitting an empty message, which reddit doesn't like.

extension/data/modules/newmodmailpro.js
@@ -140,7 +140,7 @@ function newmodmailpro () {
                 // Note: we can't use .submit() here since it will trigger
                 // the native browser submission instead of the React event listener.
                 const formElement = $body.find('.ThreadViewerReplyForm')[0];
-                formElement.dispatchEvent(new CustomEvent('submit'));
+                formElement.dispatchEvent(new Event('submit', { cancelable: false })); // cancelable: false is needed for FF
             };
 
             /**

commit 37a5b7a92c240737b201232cd08a1c397b29ce39

fix eslint errors introduced in 48212c7/#389

extension/data/modules/newmodmailpro.js
@@ -140,7 +140,7 @@ function newmodmailpro () {
                 // Note: we can't use .submit() here since it will trigger
                 // the native browser submission instead of the React event listener.
                 const formElement = $body.find('.ThreadViewerReplyForm')[0];
-                formElement.dispatchEvent(new Event('submit', { cancelable: false })); // cancelable: false is needed for FF
+                formElement.dispatchEvent(new Event('submit', {cancelable: false})); // cancelable: false is needed for FF
             };
 
             /**
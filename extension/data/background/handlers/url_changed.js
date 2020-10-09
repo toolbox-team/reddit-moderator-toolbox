b2aaaea889f56cc1389dc2f3acf5167adfee5aa9
Lars <larsjohnsen@gmail.com>
2020-10-09 17:56:52 +0200

    Use the webNavigation API
    The `tabs.onUpdated` does not trigger on changes in iframes. By using
    webNavigation's `onReferenceFragmentUpdated` and
    `onHistoryStateUpdated`, Toolbox is able to detect all changes in the
    URL, regardless of frame.
    
    Note that this requires the additional permission `webNavigation`, which
    may necessitate manual intervention by the user.

--- extension/data/background/handlers/url_changed.js
+++ extension/data/background/handlers/url_changed.js
@@ -2,14 +2,16 @@
 
 // Notifies tabs when their URL changes.
 // This is triggered in the background as there's no native event which is emitted when `history#pushState` modifies the URL.
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url) {
-        browser.tabs.sendMessage(tab.id, {action: 'tb-url-changed'})
-            .catch(error => {
-                // Receiving end errors are not really relevant to us and happen a lot for iframes and such where toolbox isn't active.
-                if (error.message !== 'Could not establish connection. Receiving end does not exist.') {
-                    console.warn('tb-url-changed: ', error.message, error);
-                }
-            });
-    }
-});
+function handleWebNavigation ({tabId, frameId}) {
+    browser.tabs.sendMessage(tabId, {action: 'tb-url-changed'})
+        .catch(error => {
+            // Receiving end errors are not really relevant to us and happen a lot for iframes and such where toolbox isn't active.
+            if (error.message !== 'Could not establish connection. Receiving end does not exist.') {
+                console.warn('tb-url-changed: ', error.message, error);
+            }
+        }, frameId);
+}
+
+const filter = {url: [{hostContains: 'reddit.com'}]};
+browser.webNavigation.onReferenceFragmentUpdated.addListener(handleWebNavigation, filter);
+browser.webNavigation.onHistoryStateUpdated.addListener(handleWebNavigation, filter);

20cc034425ac0138052463aea73a6ab9bd12476c
larsjohnsen <larsjohnsen@gmail.com>
2020-10-10 15:43:12 +0200

    Optimize watchPushState on old Reddit (#387)
    * Optimize watchPushState on old Reddit
    
    The previous solution required animation frames to be scheduled as
    long as the page was visible, preventing the CPU from idling.
    
    This change adds a passive listener in the background which send a
    message to the content script when the tab has a new URL. The
    extension API is used as there's no native Web API which triggers an
    event when the URL is modified by history#pushState.
    
    * Fix lint, name function more accurately
    
    * Catch error when notifying tab where Toolbox isn't active
    
    * Use the webNavigation API
    
    The `tabs.onUpdated` does not trigger on changes in iframes. By using
    webNavigation's `onReferenceFragmentUpdated` and
    `onHistoryStateUpdated`, Toolbox is able to detect all changes in the
    URL, regardless of frame.
    
    Note that this requires the additional permission `webNavigation`, which
    may necessitate manual intervention by the user.
    
    * Specifiy receiving frameId correctly

---
+++ extension/data/background/handlers/url_changed.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Notifies tabs when their URL changes.
+// This is triggered in the background as there's no native event which is emitted when `history#pushState` modifies the URL.
+function handleWebNavigation ({tabId, frameId}) {
+    browser.tabs.sendMessage(tabId, {action: 'tb-url-changed'}, {frameId})
+        .catch(error => {
+            // Receiving end errors are not really relevant to us and happen a lot for iframes and such where toolbox isn't active.
+            if (error.message !== 'Could not establish connection. Receiving end does not exist.') {
+                console.warn('tb-url-changed: ', error.message, error);
+            }
+        });
+}
+
+const filter = {url: [{hostContains: 'reddit.com'}]};
+browser.webNavigation.onReferenceFragmentUpdated.addListener(handleWebNavigation, filter);
+browser.webNavigation.onHistoryStateUpdated.addListener(handleWebNavigation, filter);

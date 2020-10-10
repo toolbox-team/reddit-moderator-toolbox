69389bab1a060614bd092b896e385a650b7218ca
Lars <larsjohnsen@gmail.com>
2020-10-10 11:23:11 +0200

    Specifiy receiving frameId correctly

--- extension/data/background/handlers/url_changed.js
+++ extension/data/background/handlers/url_changed.js
@@ -3,13 +3,13 @@
 // Notifies tabs when their URL changes.
 // This is triggered in the background as there's no native event which is emitted when `history#pushState` modifies the URL.
 function handleWebNavigation ({tabId, frameId}) {
-    browser.tabs.sendMessage(tabId, {action: 'tb-url-changed'})
+    browser.tabs.sendMessage(tabId, {action: 'tb-url-changed'}, {frameId})
         .catch(error => {
             // Receiving end errors are not really relevant to us and happen a lot for iframes and such where toolbox isn't active.
             if (error.message !== 'Could not establish connection. Receiving end does not exist.') {
                 console.warn('tb-url-changed: ', error.message, error);
             }
-        }, frameId);
+        });
 }
 
 const filter = {url: [{hostContains: 'reddit.com'}]};

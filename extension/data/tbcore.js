a9e0058c01ee212182de392fa61626467ef98df2
George <georgej1088@gmail.com>
2020-12-13 00:03:29 -0500

    Return no author for posts with deleted authors

--- extension/data/tbcore.js
+++ extension/data/tbcore.js
@@ -877,7 +877,7 @@ function initwrapper ({userDetails, newModSubs, cacheDetails}) {
                 const $thing = $($sender.closest('.thing')[0] || $sender);
 
                 subredditType = $thing.attr('data-subreddit-type');
-                user = $entry.find('.author:first').text() || $thing.find('.author:first').text();
+                user = $entry.find('.author:first').text() || $entry.has('> tagline') ? '[deleted]' : $thing.find('.author:first').text();
                 subreddit = $thing.attr('data-subreddit') || TBCore.post_site || $entry.find('.subreddit:first').text() || $thing.find('.subreddit:first').text() || $entry.find('.tagline .head b > a[href^="/r/"]:not(.moderator)').text();
                 permalink = $entry.find('a.bylink').attr('href') || $entry.find('.buttons:first .first a').attr('href') || $thing.find('a.bylink').attr('href') || $thing.find('.buttons:first .first a').attr('href');
                 domain = ($entry.find('span.domain:first').text() || $thing.find('span.domain:first').text()).replace('(', '').replace(')', '');

3cf3de2dd78ce571dc80d2b68cf50baefce54407
Erin <github@eritbh.me>
2022-08-20 14:28:40 -0400

    remove unused references to noteCount

--- extension/data/modules/modnotes.js
+++ extension/data/modules/modnotes.js
@@ -144,7 +144,6 @@ function getLatestModNote (subreddit, user) {
  * @param {string} data.subreddit Name of the relevant subreddit
  * @param {string} data.label Text shown in the badge if there are no notes
  * @param {object} [data.note] The most recent mod note left on the user
- * @param {number} [data.noteCount] The number of total notes for the user
  * @returns {jQuery} The created badge
  */
 function createModNotesBadge ({
@@ -152,7 +151,6 @@ function createModNotesBadge ({
     subreddit,
     label = 'NN',
     note,
-    noteCount,
 }) {
     const $badge = $(`
         <a
@@ -168,7 +166,6 @@ function createModNotesBadge ({
 
     updateModNotesBadge($badge, {
         note,
-        noteCount,
     });
 
     return $badge;

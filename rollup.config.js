860528f8a632113000eda5e53ebc7a625d4a4a81
Erin <github@eritbh.me>
2023-06-30 19:36:05 -0400

    Initial tsconfig setup

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@
 import copy from 'rollup-plugin-copy';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 
 // TODO: Pull entry point info and copied files from manifest itself
 export default ['chrome', 'firefox'].flatMap(platform => [
@@ -17,6 +18,7 @@ export default ['chrome', 'firefox'].flatMap(platform => [
         plugins: [
             nodeResolve(),
             commonjs(),
+            typescript(),
             // Copy files not processed by Rollup over to the build directory
             copy({
                 targets: [
@@ -44,6 +46,7 @@ export default ['chrome', 'firefox'].flatMap(platform => [
         plugins: [
             nodeResolve(),
             commonjs(),
+            typescript(),
         ],
     },
 ]);

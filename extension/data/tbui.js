98078b061b8b06ac3ff161c1727555da69dfa9ad
Erin <github@eritbh.me>
2023-08-07 22:00:53 -0400

    Add pager alternatives that work with generators

--- extension/data/tbui.js
+++ extension/data/tbui.js
@@ -1727,84 +1727,123 @@ export function makeCommentThread (jsonInput, commentOptions) {
 }
 
 /**
- * Creates a jQuery element that dynamically displays paginated content.
+ * Creates a jQuery element that displays paginated content provided by a
+ * generator. Lazy loading is supported.
  * @param {object} options Options for the pager
- * @param {number} options.pageCount The number of pages to present
+ * @param {boolean} [options.lazy=true] If `false`, the page generator will be
+ * iterated to completion immediately, and controls for all pages will be
+ * visible from the start. If `true`, the pager will display a "next page"
+ * button which loads new pages from the generator one at a time until it is
+ * exhausted.
  * @param {string} options.controlPosition Where to display the pager's
  * controls, either 'top' or 'bottom'
- * @param {TBui~pagerCallback} contentFunction A function which generates
- * content for a given page
- * @returns {jQuery}
+ * @param {AsyncGenerator<string | JQuery, void, unknown>} contentGenerator A
+ * generator which yields content for each page
+ * @returns {JQuery}
  */
-// TODO: optionally support caching calls to the content function to avoid wasting time regenerating identical pages
-export function pager ({pageCount, controlPosition = 'top'}, contentFunction) {
+export function progressivePager ({
+    lazy = true,
+    controlPosition = 'top',
+}, contentGenerator) {
     // Create elements for the content view and the pagination controls
     const $pagerContent = $('<div class="tb-pager-content"/>');
     const $pagerControls = $('<div class="tb-pager-controls"/>');
 
-    // An array of all the button elements that could be displayed, one for each page
-    const buttons = [];
+    // An array of all the pages that could be displayed
+    const pages = [];
+    // If true, the generator is finished and there will be no more pages added
+    let pagesDone = false;
+
+    function makeButton (i) {
+        // Create the button, translating 0-indexed to 1-indexed pages for human eyes
+        const $button = $(button(i + 1, 'tb-pager-control'));
+        $button.attr('data-page-index', i);
+
+        // When the button is clicked, go to the correct page
+        $button.on('click', () => {
+            loadPage(i);
+        });
+
+        return $button;
+    }
+
+    async function getPage (i) {
+        if (i < pages.length) {
+            return pages[i];
+        }
+        for (let j = pages.length; j <= i; j += 1) {
+            const {value, done} = await contentGenerator.next();
+            if (done) {
+                pagesDone = true;
+            } else {
+                pages.push(value);
+                return value;
+            }
+        }
+    }
 
     // A function that refreshes the displayed buttons based on the selected page
-    function loadPage (pageIndex) {
-        // If we have more than 10 pages, refresh the buttons that are being actively displayed
-        if (pageCount > 10) {
-            // Empty the existing buttons out, using .detach to maintain event listeners, then using .empty() to
-            // remove the text left behind (thanks jQuery)
-            $pagerControls.children().detach();
-            $pagerControls.empty();
+    async function loadPage (pageIndex) {
+        // Get the content for this page
+        let pageContent = await getPage(pageIndex);
 
-            // Add the buttons in the center
-            const leftBound = Math.max(pageIndex - 4, 0);
-            const rightBound = Math.min(pageIndex + 4, pageCount - 1);
-            for (let buttonIndex = leftBound; buttonIndex <= rightBound; buttonIndex += 1) {
-                $pagerControls.append(buttons[buttonIndex]);
-            }
+        // If we just tried to fetch a page and hit the end of the generator
+        // instead, display the last page instead - but continue with the
+        // update to make sure the buttons are up-to-date
+        if (pagesDone && pageIndex >= pages.length) {
+            pageIndex = pages.length - 1;
+            pageContent = await getPage(pageIndex);
+        }
 
-            // Add the first and last page buttons, along with "..." between them and the other buttons if there's
-            // distance between them
-            if (leftBound > 1) {
-                $pagerControls.prepend('...');
-            }
-            if (leftBound > 0) {
-                $pagerControls.prepend(buttons[0]);
-            }
-            if (rightBound < pageCount - 2) {
-                $pagerControls.append('...');
-            }
-            if (rightBound < pageCount - 1) {
-                $pagerControls.append(buttons[pageCount - 1]);
-            }
-        } else if ($pagerControls.children().length === 0) {
-            // If we have less than 10 items, then we only need to refresh the buttons the first time they're loaded
-            buttons.forEach($button => $pagerControls.append($button));
+        // Display content for the new page
+        $pagerContent.empty().append(pageContent);
+
+        // Empty the existing buttons out, using .detach to maintain event listeners, then using .empty() to
+        // remove the text left behind (thanks jQuery)
+        $pagerControls.children().detach();
+        $pagerControls.empty();
+
+        let leftBound = 0;
+        let rightBound = pages.length - 1;
+
+        // If we have more than 10 pages, only display first and last buttons,
+        // plus some around where we are now
+        if (pages.length > 10) {
+            leftBound = Math.max(pageIndex - 4, 0);
+            rightBound = Math.min(pageIndex + 4, pages.length - 1);
+        }
+
+        // Add all the buttons within the bounds
+        for (let buttonIndex = leftBound; buttonIndex <= rightBound; buttonIndex += 1) {
+            $pagerControls.append(makeButton(buttonIndex));
+        }
+
+        // Add the first and last page buttons, along with "..." between them and the other buttons if there's
+        // distance between them
+        if (leftBound > 1) {
+            $pagerControls.prepend('...');
+        }
+        if (leftBound > 0) {
+            $pagerControls.prepend(makeButton(0));
+        }
+        if (rightBound < pages.length - 2) {
+            $pagerControls.append('...');
+        }
+        if (rightBound < pages.length - 1) {
+            $pagerControls.append(makeButton(pages.length - 1));
+        }
+
+        // Include a button to load another page if we still can
+        if (!pagesDone) {
+            const $nextPageButton = makeButton(pages.length);
+            $nextPageButton.text('load more...');
+            $nextPageButton.attr('title', 'load next page');
+            $pagerControls.append($nextPageButton);
         }
 
         // Move selection to the correct button
         $pagerControls.children().toggleClass('tb-pager-control-active', false);
-        buttons[pageIndex].toggleClass('tb-pager-control-active', true);
-
-        // Generate and display content for the new page
-        /**
-         * @callback TBui~pagerCallback
-         * @param {number} page The zero-indexed number of the page to generate
-         * @returns {string | jQuery} The content of the page
-         */
-        $pagerContent.empty().append(contentFunction(pageIndex));
-    }
-
-    // Create all the buttons
-    for (let page = 0; page < pageCount; page += 1) {
-        // Create the button, translating 0-indexed to 1-indexed pages fur human eyes
-        const $button = $(button(page + 1, 'tb-pager-control'));
-
-        // When the button is clicked, go to the correct page
-        $button.on('click', () => {
-            loadPage(page);
-        });
-
-        // Add to the array
-        buttons.push($button);
+        $pagerControls.find(`[data-page-index="${pageIndex}"]`).toggleClass('tb-pager-control-active', true);
     }
 
     // Construct the pager itself
@@ -1818,10 +1857,97 @@ export function pager ({pageCount, controlPosition = 'top'}, contentFunction) {
         throw new TypeError('Invalid controlPosition');
     }
 
-    // Preload the content for the first page
-    loadPage(0);
+    (async () => {
+        // Preload the content for the first page
+        await loadPage(0);
+
+        // if we're not lazy, preload all pages from the generator
+        if (!lazy) {
+            (async () => {
+                while (!pagesDone) {
+                    await getPage(pages.length);
+                }
+            })();
+        }
+    })();
 
     return $pager;
+}
+
+/**
+ * Creates a jQuery element that displays paginated content provided by a
+ * generator. The generator outputs markup for individual items, and output is
+ * aggregated into pages, with the given number of items per page. Lazy loading
+ * of items is supported.
+ * @param {object} options Options for the pager
+ * @param {boolean} [options.lazy=true] If `false`, the item generator will be
+ * iterated to completion immediately, and controls for all pages will be
+ * visible from the start. If `true`, the pager will display a "next page"
+ * button which loads new items from the generator one at a time until it is
+ * exhausted.
+ * @param {string} options.controlPosition Where to display the pager's
+ * controls, either 'top' or 'bottom'
+ * @param {number} options.perPage The number of items from the item generator
+ * to display on each page of the pager
+ * @param {number} [options.wrapper] Used to provide custom wrapper markup for
+ * each page of items
+ * @param {AsyncGenerator<string | JQuery, void, unknown>} itemGenerator A
+ * generator which yields content for each item displayed on the page
+ * @returns {JQuery}
+ */
+export function progressivePagerForItems ({
+    lazy = true,
+    controlPosition,
+    perPage,
+    wrapper = '<div>',
+}, itemGenerator) {
+    return progressivePager({
+        lazy,
+        controlPosition,
+    }, (async function * () {
+        let $wrapper = $(wrapper);
+        // track size separately from $wrapper.children().length since
+        // sometimes the wrapper has other children we don't control
+        let size = 0;
+
+        // stuff items into pages and yield each page when it's full
+        for await (const item of itemGenerator) {
+            $wrapper.append(item);
+            size += 1;
+            if (size >= perPage) {
+                yield $wrapper;
+                $wrapper = $(wrapper);
+                size = 0;
+            }
+        }
+
+        // if there's an incomplete page left over when we're done, yield it
+        if (size) {
+            yield $wrapper;
+        }
+    })());
+}
+
+/**
+ * Creates a jQuery element that dynamically displays paginated content.
+ * @param {object} options Options for the pager
+ * @param {number} options.pageCount The number of pages to present
+ * @param {string} options.controlPosition Where to display the pager's
+ * controls, either 'top' or 'bottom'
+ * @param {TBui~pagerCallback} contentFunction A function which generates
+ * content for a given page
+ * @returns {jQuery}
+ */
+export function pager ({pageCount, controlPosition = 'top'}, contentFunction) {
+    return progressivePager({
+        lazy: false,
+        controlPosition,
+        contentGenerator: (function * () {
+            for (let i = 0; i < pageCount; i += 1) {
+                yield contentFunction(i);
+            }
+        })(),
+    });
 }
 
 /**

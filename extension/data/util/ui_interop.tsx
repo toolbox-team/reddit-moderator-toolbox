6f7acface31e9aa5b8ea6537312006e939f65d6d
ewin <hi@ewin.me>
2024-09-05 01:40:32 -0400

    create helper for making document body portals into shadow roots with our styles

--- extension/data/util/ui_interop.tsx
+++ extension/data/util/ui_interop.tsx
@@ -7,7 +7,12 @@ import {ReactNode, useEffect, useRef} from 'react';
 import {createRoot} from 'react-dom/client';
 import {onDOMAttach} from './dom';
 
+import {createPortal} from 'react-dom';
 import browser from 'webextension-polyfill';
+
+const bundledStylesheetLink = (
+    <link rel='stylesheet' type='text/css' href={browser.runtime.getURL('data/bundled.css')} />
+);
 
 /**
  * Returns a <div> DOM element which renders the given React/JSX content when
@@ -20,12 +25,28 @@ export function reactRenderer (content: ReactNode) {
     onDOMAttach(contentShadowHost, () => {
         createRoot(shadowRoot).render([
             // pull in bundled stylesheet which contains all the CSS module code for our React components
-            <link rel='stylesheet' type='text/css' href={browser.runtime.getURL('data/bundled.css')} />,
+            bundledStylesheetLink,
             // then insert the actual content
             content,
         ]);
     });
     return contentShadowHost;
+}
+
+/**
+ * Creates a portal into the shadow root of a new <div> which is placed in the
+ * bottom of <body>. The shadow root will also contain the necessary <link> to
+ * include the bundled stylesheet, allowing components inside to have their
+ * styles render.
+ */
+export function createBodyShadowPortal (children: ReactNode, key?: null | string) {
+    const shadowHost = document.createElement('div');
+    shadowHost.classList.add('tb-react-shadow-portal');
+    const shadowRoot = shadowHost.attachShadow({mode: 'open'});
+
+    document.body.append(shadowHost);
+    // stylesheet injection shenanigans
+    return createPortal([bundledStylesheetLink, children], shadowRoot, key);
 }
 
 /** A React component which renders a jQuery element as its contents. */
@@ -41,4 +62,13 @@ export function JQueryRenderer ({content}: {content: JQuery}) {
         }
     }, [content]);
     return <div ref={target} />;
+}
+
+// this isn't really an "interop" thing but whatever it lives here for now
+/**
+ * Constructs a `className` value from a bunch of passed in things that could
+ * be either strings or nothing.
+ */
+export function classes (...stuff: (string | false | null | undefined)[]) {
+    return stuff.flat().filter(thing => !!thing).join(' ');
 }

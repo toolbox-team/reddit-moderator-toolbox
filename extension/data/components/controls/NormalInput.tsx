39770e4964b28cef9023cfc22165570512bd9987
Erin <github@eritbh.me>
2024-10-23 18:40:10 -0400

    use forwardRef to define NormalInput to make things work again

--- extension/data/components/controls/NormalInput.tsx
+++ extension/data/components/controls/NormalInput.tsx
@@ -1,17 +1,22 @@
-import {type ComponentPropsWithoutRef} from 'react';
+import {type ComponentPropsWithoutRef, forwardRef} from 'react';
 import {classes} from '../../util/ui_interop';
 import css from './NormalInput.module.css';
 
 // TODO: this is a terrible name
-export const NormalInput = ({
+export const NormalInput = forwardRef<HTMLInputElement>(({
     inFooter,
     className,
     ...props
 }: ComponentPropsWithoutRef<'input'> & {
     inFooter?: boolean;
-}) => (
+}, ref) => (
     <input
-        className={classes(css.normalInput, inFooter && css.inWindowFooter, className)}
+        className={classes(
+            css.normalInput,
+            inFooter && css.inWindowFooter,
+            className,
+        )}
         {...props}
+        ref={ref}
     />
-);
+));

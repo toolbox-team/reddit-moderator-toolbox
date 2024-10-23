f69ff5e22471d19ec1a8aa125138abc93489ef51
Erin <github@eritbh.me>
2024-10-23 18:03:43 -0400

    update control component index for easier importing

--- extension/data/components/controls/index.ts
+++ extension/data/components/controls/index.ts
@@ -1,4 +1,7 @@
 export * from './ActionButton';
+export * from './ActionSelect';
+export * from './BracketButton';
 export * from './GeneralButton';
 export * from './Icon';
+export * from './NormalInput';
 export * from './RelativeTime';

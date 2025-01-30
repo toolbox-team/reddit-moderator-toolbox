3b2b8b534e428ddc51dbe109e60cccc760c4b4c6
Erin <github@eritbh.me>
2025-01-30 14:57:30 -0500

    reimplement `useSetting` in terms of settings store

--- extension/data/hooks.ts
+++ extension/data/hooks.ts
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react';
-import {getSettingAsync} from './tbstorage';
+import {useSelector} from 'react-redux';
+import {RootState} from './store';
 
 /** Hook to get the return value of a promise. */
 export const useFetched = <T>(promise: Promise<T>) => {
@@ -23,5 +24,15 @@ export const useFetched = <T>(promise: Promise<T>) => {
 
 /** Hook to get a Toolbox setting. */
 export const useSetting = (moduleName: string, settingName: string, defaultValue: any) => {
-    return useFetched(getSettingAsync(moduleName, settingName, defaultValue));
+    const savedValue = useSelector((state: RootState) => state.settings.values[`Toolbox.${moduleName}.${settingName}`]);
+
+    // Return the given default value if the setting doesn't have a value (i.e.
+    // is `undefined`) *or* if the setting's value is `null` (mirroring the
+    // implementation of `getSetting` in `tbstorage.js`, which says that `null`
+    // is never a valid value for any setting)
+    if (savedValue == null) {
+        return defaultValue;
+    }
+
+    return savedValue;
 };

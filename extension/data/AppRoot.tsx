8561283803aded738f8b67ae87d7f60739d26130
Erin <github@eritbh.me>
2025-01-30 14:45:15 -0500

    let's make the redux store actually make sense and use actions and reducers properly

--- extension/data/AppRoot.tsx
+++ extension/data/AppRoot.tsx
@@ -1,33 +1,15 @@
 import {Provider} from 'react-redux';
 
-import {storeFromInitialSettings} from './store';
+import store from './store';
 
 import {PageNotificationContainer} from './components/PageNotificationContainer';
 import {TextFeedbackContainer} from './components/TextFeedbackContainer';
-import {useFetched} from './hooks';
-import {getSettings} from './tbstorage';
 
-export default function () {
-    // We don't have a store premade - we have to fetch our settings first, then
-    // use those values to prepopulate the store
-    const initialSettings = useFetched(getSettings());
-
-    // Render nothing if we don't have settings yet
-    if (initialSettings == null) {
-        return <></>;
-    }
-
-    // Create the store on the fly, providing the initial settings values as
-    // preloaded state
-    const store = storeFromInitialSettings(initialSettings);
-
-    // Render the app
-    return (
-        <Provider store={store}>
-            <div className='tb-app-root'>
-                <PageNotificationContainer />
-                <TextFeedbackContainer />
-            </div>
-        </Provider>
-    );
-}
+export default () => (
+    <Provider store={store}>
+        <div className='tb-app-root'>
+            <PageNotificationContainer />
+            <TextFeedbackContainer />
+        </div>
+    </Provider>
+);
--- extension/data/store/index.ts
+++ extension/data/store/index.ts
@@ -1,5 +1,5 @@
 import {combineReducers, configureStore, type ThunkAction, type UnknownAction} from '@reduxjs/toolkit';
-import settingsReducer, {SettingsState} from './settingsSlice';
+import settingsReducer, {loadSettings} from './settingsSlice';
 import textFeedbackReducer from './textFeedbackSlice';
 
 const rootReducer = combineReducers({
@@ -7,25 +7,17 @@ const rootReducer = combineReducers({
     settings: settingsReducer,
 });
 
-// We want to initialize the store with the current values of all our settings.
-// However, settings need to be fetched asynchronously, and `preloadedState`
-// doesn't work correctly with promises - we don't want promises in our state,
-// we want the store's construction to be deferred until the promises resolve.
-// So instead of exporting a configured `store` as a default export, we export a
-// function which can be used to create a store from a given initial state. In
-// `AppRoot.tsx`, we fetch the initial setting values, and only once we have
-// them do we call this function and provide the store context to the app.
-export const storeFromInitialSettings = (settings: SettingsState) =>
-    configureStore({
-        reducer: rootReducer,
-        preloadedState: {
-            settings,
-        },
-    });
+const store = configureStore({
+    reducer: rootReducer,
+});
+export default store;
+
+// Attempt to load settings from storage immediately
+store.dispatch(loadSettings());
 
 // TS concerns - see https://redux.js.org/usage/usage-with-typescript#define-root-state-and-dispatch-types
 export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = ReturnType<typeof storeFromInitialSettings>['dispatch'];
+export type AppDispatch = typeof store.dispatch;
 export type AppThunk<ReturnType = void> = ThunkAction<
     ReturnType,
     RootState,
